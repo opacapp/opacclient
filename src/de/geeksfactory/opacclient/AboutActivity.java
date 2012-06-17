b6fff80d58492c08638dc677a7895b2ebf7b6346
Raphael Michel <webmaster@raphaelmichel.de>
2012-06-18 00:14:24 +0200

    Extending about screen

--- src/de/geeksfactory/opacclient/AboutActivity.java
+++ src/de/geeksfactory/opacclient/AboutActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class AboutActivity extends Activity {
@@ -22,12 +23,14 @@ public class AboutActivity extends Activity {
 					"<a href='mailto:raphael@geeksfactory.de'>raphael@geeksfactory.de</a><br /><br />" +
 					"Veröffentlicht von der geek's factory<br />" +
 					"<a href='http://www.geeksfactory.de'>www.geeksfactory.de</a><br /><br />"+
-					"Diese App ist Open Source Software.<br /><br />"+
+					"Diese App ist freie Software, den Source Code gibt es hier:<br />"+
+					"<a href='https://github.com/raphaelm/opacclient'>github.com/raphaelm/opacclient</a><br /><br />"+
 					"Die Icons stammen aus dem Human O2 Set von Oliver Scholtz<br /><br />"+
 					"Gedacht zur Bedienung von BOND Web-Opacs V2.6<br /><br />"+
 					"Danke an Céline A.<br />"+
 					"Die App verwendet <a href='http://jsoup.org'>jsoup</a> und mag es.<br />"
 					));
+			tvAbout.setMovementMethod(LinkMovementMethod.getInstance());
 		} catch (NameNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
