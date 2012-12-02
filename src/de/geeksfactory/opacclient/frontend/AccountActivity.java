bb214afa1a9f94d01961f94220fdbf11ff37fa1a
Raphael Michel <webmaster@raphaelmichel.de>
2012-12-02 18:31:22 +0100

    Account-Auswahl in Mutterklasse ausgelagert

--- src/de/geeksfactory/opacclient/frontend/AccountActivity.java
+++ src/de/geeksfactory/opacclient/frontend/AccountActivity.java
@@ -1,29 +1,21 @@
 package de.geeksfactory.opacclient.frontend;
 
-import java.io.IOException;
 import java.util.List;
-
-import org.json.JSONException;
 
 import android.app.NotificationManager;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
 import android.text.Html;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TableLayout;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TableLayout.LayoutParams;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -35,13 +27,10 @@ import com.actionbarsherlock.view.MenuItem;
 import de.geeksfactory.opacclient.OpacClient;
 import de.geeksfactory.opacclient.OpacTask;
 import de.geeksfactory.opacclient.R;
-import de.geeksfactory.opacclient.objects.Account;
-import de.geeksfactory.opacclient.storage.AccountDataSource;
 
 public class AccountActivity extends OpacActivity {
 
 	protected ProgressDialog dialog;
-	protected AlertDialog adialog;
 
 	public static int STATUS_SUCCESS = 0;
 	public static int STATUS_NOUSER = 1;
@@ -89,49 +78,6 @@ public class AccountActivity extends OpacActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	public void selectaccount() {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		// Get the layout inflater
-		LayoutInflater inflater = getLayoutInflater();
-
-		View view = inflater.inflate(R.layout.account_add_liblist_dialog, null);
-
-		ListView lv = (ListView) view.findViewById(R.id.lvBibs);
-		AccountDataSource data = new AccountDataSource(this);
-		data.open();
-		final List<Account> accounts = data.getAllAccounts();
-		data.close();
-		AccountListAdapter adapter = new AccountListAdapter(this, accounts);
-		lv.setAdapter(adapter);
-		lv.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-
-				((OpacClient) getApplication()).setAccount(accounts.get(position).getId());
-				onResume();
-				adialog.dismiss();
-			}
-		});
-
-		builder.setView(view).setNegativeButton(R.string.cancel,
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				}).setNeutralButton(R.string.accounts_edit,
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.dismiss();
-						Intent intent = new Intent(AccountActivity.this,
-								AccountListActivity.class);
-						startActivity(intent);
-					}
-				});
-		adialog = builder.create();
-		adialog.show();
 	}
 
 	@Override
@@ -230,7 +176,8 @@ public class AccountActivity extends OpacActivity {
 			SharedPreferences sp = PreferenceManager
 					.getDefaultSharedPreferences(AccountActivity.this.app);
 			try {
-				List<List<String[]>> res = app.getApi().account(((OpacClient)getApplication()).getAccount());
+				List<List<String[]>> res = app.getApi().account(
+						((OpacClient) getApplication()).getAccount());
 				success = true;
 				return res;
 			} catch (java.net.UnknownHostException e) {
