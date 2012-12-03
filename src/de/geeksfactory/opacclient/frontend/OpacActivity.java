b58f673d170eaf68bbb7150de16bc414a95b5c79
Raphael Michel <webmaster@raphaelmichel.de>
2012-12-03 22:05:08 +0100

    Vorbestellungen wieder aktiviert

--- src/de/geeksfactory/opacclient/frontend/OpacActivity.java
+++ src/de/geeksfactory/opacclient/frontend/OpacActivity.java
@@ -2,6 +2,8 @@ package de.geeksfactory.opacclient.frontend;
 
 import java.util.List;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -93,6 +96,78 @@ public abstract class OpacActivity extends SherlockActivity {
 
 	public void selectaccount() {
 		selectaccount(null);
+	}
+
+	public class MetaAdapter extends ArrayAdapter<ContentValues> {
+
+		private List<ContentValues> objects;
+		private int spinneritem;
+
+		@Override
+		public View getDropDownView(int position, View contentView,
+				ViewGroup viewGroup) {
+			View view = null;
+
+			if (objects.get(position) == null) {
+				LayoutInflater layoutInflater = (LayoutInflater) getContext()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = layoutInflater
+						.inflate(R.layout.simple_spinner_dropdown_item,
+								viewGroup, false);
+				return view;
+			}
+
+			ContentValues item = objects.get(position);
+
+			if (contentView == null) {
+				LayoutInflater layoutInflater = (LayoutInflater) getContext()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = layoutInflater
+						.inflate(R.layout.simple_spinner_dropdown_item,
+								viewGroup, false);
+			} else {
+				view = contentView;
+			}
+
+			TextView tvText = (TextView) view.findViewById(android.R.id.text1);
+			tvText.setText(item.getAsString("value"));
+			return view;
+		}
+
+		@Override
+		public View getView(int position, View contentView, ViewGroup viewGroup) {
+			View view = null;
+
+			if (objects.get(position) == null) {
+				LayoutInflater layoutInflater = (LayoutInflater) getContext()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = layoutInflater.inflate(spinneritem,
+						viewGroup, false);
+				return view;
+			}
+
+			ContentValues item = objects.get(position);
+
+			if (contentView == null) {
+				LayoutInflater layoutInflater = (LayoutInflater) getContext()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = layoutInflater.inflate(spinneritem,
+						viewGroup, false);
+			} else {
+				view = contentView;
+			}
+
+			TextView tvText = (TextView) view.findViewById(android.R.id.text1);
+			tvText.setText(item.getAsString("value"));
+			return view;
+		}
+
+		public MetaAdapter(Context context, List<ContentValues> objects, int spinneritem) {
+			super(context, R.layout.simple_spinner_item, objects);
+			this.objects = objects;
+			this.spinneritem = spinneritem;
+		}
+
 	}
 
 	public void selectaccount(final AccountSelectedListener listener) {
