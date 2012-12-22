07b641287cf9c94b8b9f7d68725c744400a04c54
Raphael Michel <webmaster@raphaelmichel.de>
2012-12-23 00:26:10 +0100

    Starte FrontpageActivity nach Absturz

--- src/de/geeksfactory/opacclient/frontend/ErrorActivity.java
+++ src/de/geeksfactory/opacclient/frontend/ErrorActivity.java
@@ -51,7 +51,7 @@ public class ErrorActivity extends SherlockActivity {
 			public void onClick(View v) {
 				PendingIntent RESTART_INTENT = PendingIntent.getActivity(
 						getBaseContext(), 0, new Intent(ErrorActivity.this,
-								SearchActivity.class), getIntent().getFlags());
+								FrontpageActivity.class), getIntent().getFlags());
 				AlarmManager mgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 				mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1000,
 						RESTART_INTENT);
