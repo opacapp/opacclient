25f610334b7d59e66917ef7907d88eb799b1d1a2
Raphael Michel <webmaster@raphaelmichel.de>
2013-01-17 14:36:14 +0100

    Navigation header logic

--- src/de/geeksfactory/opacclient/frontend/NavigationFragment.java
+++ src/de/geeksfactory/opacclient/frontend/NavigationFragment.java
@@ -1,6 +1,10 @@
 package de.geeksfactory.opacclient.frontend;
 
+import org.json.JSONException;
+
+import de.geeksfactory.opacclient.OpacClient;
 import de.geeksfactory.opacclient.R;
+import de.geeksfactory.opacclient.objects.Library;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NavigationFragment extends Fragment {
 
@@ -80,5 +85,36 @@ public class NavigationFragment extends Fragment {
 
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+
+		reload();
+	}
+
+	public void reload() {
+		Library lib = ((OpacClient) getActivity().getApplication())
+				.getLibrary();
+		if (lib != null) {
+			TextView tvBn = (TextView) getView().findViewById(R.id.tvLibrary);
+			if (lib.getTitle() != null && !lib.getTitle().equals("null"))
+				tvBn.setText(lib.getCity() + " · " + lib.getTitle());
+			else
+				tvBn.setText(lib.getCity());
+
+			try {
+				if (lib.getData().getString("information") != null) {
+					if (!lib.getData().getString("information").equals("null")) {
+						getView().findViewById(R.id.llNavInfo).setVisibility(
+								View.VISIBLE);
+					} else {
+						getView().findViewById(R.id.llNavInfo).setVisibility(
+								View.GONE);
+					}
+				} else {
+					getView().findViewById(R.id.llNavInfo).setVisibility(
+							View.GONE);
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
