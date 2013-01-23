673f3dfc94074ee786bbf5b58ab4e8121a05bab6
Raphael Michel <webmaster@raphaelmichel.de>
2013-01-23 15:40:38 +0100

    "Add account" is now an action instead of a button

--- src/de/geeksfactory/opacclient/frontend/AccountListActivity.java
+++ src/de/geeksfactory/opacclient/frontend/AccountListActivity.java
@@ -20,6 +20,7 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
 import de.geeksfactory.opacclient.OpacClient;
@@ -39,62 +40,57 @@ public class AccountListActivity extends SherlockActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_account_list);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
 
-		Button btAdd = (Button) findViewById(R.id.btAdd);
-		btAdd.setOnClickListener(new OnClickListener() {
+	public void add() {
 
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+				AccountListActivity.this);
+		// Get the layout inflater
+		LayoutInflater inflater = getLayoutInflater();
+
+		View view = inflater.inflate(R.layout.account_add_liblist_dialog, null);
+
+		ListView lv = (ListView) view.findViewById(R.id.lvBibs);
+		try {
+			libraries = ((OpacClient) getApplication()).getLibraries();
+		} catch (IOException e) {
+			ACRA.getErrorReporter().handleException(e);
+		} catch (JSONException e) {
+			ACRA.getErrorReporter().handleException(e);
+		}
+		lv.setAdapter(new LibraryListAdapter(AccountListActivity.this,
+				libraries));
+		lv.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onClick(View v) {
-				AlertDialog.Builder builder = new AlertDialog.Builder(
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				AccountDataSource data = new AccountDataSource(
 						AccountListActivity.this);
-				// Get the layout inflater
-				LayoutInflater inflater = getLayoutInflater();
+				data.open();
+				Account acc = new Account();
+				acc.setLibrary(libraries.get(position).getIdent());
+				acc.setLabel(getString(R.string.default_account_name));
+				long insertedid = data.addAccount(acc);
+				data.close();
+				dialog.dismiss();
 
-				View view = inflater.inflate(
-						R.layout.account_add_liblist_dialog, null);
-
-				ListView lv = (ListView) view.findViewById(R.id.lvBibs);
-				try {
-					libraries = ((OpacClient) getApplication()).getLibraries();
-				} catch (IOException e) {
-					ACRA.getErrorReporter().handleException(e);
-				} catch (JSONException e) {
-					ACRA.getErrorReporter().handleException(e);
-				}
-				lv.setAdapter(new LibraryListAdapter(AccountListActivity.this,
-						libraries));
-				lv.setOnItemClickListener(new OnItemClickListener() {
-					@Override
-					public void onItemClick(AdapterView<?> parent, View view,
-							int position, long id) {
-						AccountDataSource data = new AccountDataSource(
-								AccountListActivity.this);
-						data.open();
-						Account acc = new Account();
-						acc.setLibrary(libraries.get(position).getIdent());
-						acc.setLabel(getString(R.string.default_account_name));
-						long insertedid = data.addAccount(acc);
-						data.close();
-						dialog.dismiss();
-
-						Intent i = new Intent(AccountListActivity.this,
-								AccountEditActivity.class);
-						i.putExtra("id", insertedid);
-						i.putExtra("adding", true);
-						startActivity(i);
-					}
-				});
-
-				builder.setView(view).setNegativeButton(R.string.cancel,
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								dialog.cancel();
-							}
-						});
-				dialog = builder.create();
-				dialog.show();
+				Intent i = new Intent(AccountListActivity.this,
+						AccountEditActivity.class);
+				i.putExtra("id", insertedid);
+				i.putExtra("adding", true);
+				startActivity(i);
 			}
 		});
+
+		builder.setView(view).setNegativeButton(R.string.cancel,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		dialog = builder.create();
+		dialog.show();
 	}
 
 	private void refreshLv() {
@@ -139,8 +135,17 @@ public class AccountListActivity extends SherlockActivity {
 	}
 
 	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.activity_account_list, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
+		case R.id.action_add:
+			add();
+			return true;
 		case android.R.id.home:
 			finish();
 			return true;
