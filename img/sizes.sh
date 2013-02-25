a4a6ceb2bbc0d78317cce1f799c30c94190bf0ca
Raphael Michel <webmaster@raphaelmichel.de>
2013-02-25 14:08:42 +0100

    Remove some obsolete images

--- img/sizes.sh
+++ img/sizes.sh
@@ -1,17 +1,6 @@
-cp ic_launcher.png ic_launcher_big.png                                   
-convert ic_launcher.png -resize 512x512 ../web/graphics/launcher.png
-
-for f in type*.png ic_launcher.png; do
+for f in type*.png; do
 	convert $f -thumbnail 36x36 ../res/drawable-ldpi/$f;
 	convert $f -thumbnail 48x48 ../res/drawable-mdpi/$f;
 	convert $f -thumbnail 72x72 ../res/drawable-hdpi/$f;
 	convert $f -thumbnail 96x96 ../res/drawable-xhdpi/$f;
 done;
-
-for f in navigate*.png; do
-	convert $f -thumbnail x80 ../res/drawable-xhdpi/$f;
-	convert $f -thumbnail x60 ../res/drawable-hdpi/$f;
-	convert $f -thumbnail x40 ../res/drawable-mdpi/$f;
-	convert $f -thumbnail x30 ../res/drawable-ldpi/$f;
-done;
-
