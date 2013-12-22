413a534c84fb9d2fb153daf94be2e2a19dc0c264
Raphael Michel <mail@raphaelmichel.de>
2013-12-22 19:26:05 +0100

    New launcher icons

--- img/sizes.sh
+++ img/sizes.sh
@@ -4,3 +4,9 @@ for f in type*.png; do
 	convert $f -thumbnail 72x72 ../res/drawable-hdpi/$f;
 	convert $f -thumbnail 96x96 ../res/drawable-xhdpi/$f;
 done;
+for f in ic_launcher.png; do
+	convert $f -thumbnail 36x36 ../res/drawable-ldpi/$f;
+	convert $f -thumbnail 48x48 ../res/drawable-mdpi/$f;
+	convert $f -thumbnail 72x72 ../res/drawable-hdpi/$f;
+	convert $f -thumbnail 96x96 ../res/drawable-xhdpi/$f;
+done;
