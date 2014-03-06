97d4525734002d3b11817684621adf0c6678d8e0
Raphael Michel <mail@raphaelmichel.de>
2014-03-06 17:54:57 +0100

    Code format

--- src/de/geeksfactory/opacclient/frontend/OpacActivity.java
+++ src/de/geeksfactory/opacclient/frontend/OpacActivity.java
@@ -65,17 +65,17 @@ public abstract class OpacActivity extends Activity {
 	protected OpacClient app;
 	protected AlertDialog adialog;
 	protected AccountDataSource aData;
-	
+
 	private int selectedItemPos;
-	
+
 	private NavigationAdapter navAdapter;
 	private ListView drawerList;
 	private DrawerLayout drawerLayout;
 	private ActionBarDrawerToggle drawerToggle;
 	private CharSequence mTitle;
-	
+
 	private List<Account> accounts;
-	
+
 	protected Fragment fragment;
 	protected boolean hasDrawer = false;
 
@@ -86,10 +86,10 @@ public abstract class OpacActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(getContentView());
 		app = (OpacClient) getApplication();
-		
+
 		aData = new AccountDataSource(this);
 		setupDrawer();
 
@@ -100,58 +100,66 @@ public abstract class OpacActivity extends Activity {
 	private void setupDrawer() {
 		SharedPreferences sp = PreferenceManager
 				.getDefaultSharedPreferences(this);
-		
+
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		if(drawerLayout != null) {
+		if (drawerLayout != null) {
 			hasDrawer = true;
-			drawerToggle = new ActionBarDrawerToggle(
-	                this,                  /* host Activity */
-	                drawerLayout,         /* DrawerLayout object */
-	                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
-	                R.string.drawer_open,  /* "open drawer" description */
-	                R.string.drawer_close  /* "close drawer" description */
-	                ) {
-	
-	            /** Called when a drawer has settled in a completely closed state. */
-	            public void onDrawerClosed(View view) {
-	                super.onDrawerClosed(view);
-	                getSupportActionBar().setTitle(mTitle);
-	            }
-	
-	            /** Called when a drawer has settled in a completely open state. */
-	            public void onDrawerOpened(View drawerView) {
-	                super.onDrawerOpened(drawerView);
-	                getSupportActionBar().setTitle(app.getResources().getString(R.string.app_name));
-	                if (getCurrentFocus() != null) {
+			drawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+			drawerLayout, /* DrawerLayout object */
+			R.drawable.ic_drawer, /* nav drawer icon to replace 'Up' caret */
+			R.string.drawer_open, /* "open drawer" description */
+			R.string.drawer_close /* "close drawer" description */
+			) {
+
+				/**
+				 * Called when a drawer has settled in a completely closed
+				 * state.
+				 */
+				public void onDrawerClosed(View view) {
+					super.onDrawerClosed(view);
+					getSupportActionBar().setTitle(mTitle);
+				}
+
+				/** Called when a drawer has settled in a completely open state. */
+				public void onDrawerOpened(View drawerView) {
+					super.onDrawerOpened(drawerView);
+					getSupportActionBar().setTitle(
+							app.getResources().getString(R.string.app_name));
+					if (getCurrentFocus() != null) {
 						InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 						imm.hideSoftInputFromWindow(getCurrentFocus()
 								.getWindowToken(), 0);
 					}
-	            }
-	        };
-	
-	        // Set the drawer toggle as the DrawerListener
-	        drawerLayout.setDrawerListener(drawerToggle);
-	        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-	        getSupportActionBar().setHomeButtonEnabled(true);
-			
+				}
+			};
+
+			// Set the drawer toggle as the DrawerListener
+			drawerLayout.setDrawerListener(drawerToggle);
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+			getSupportActionBar().setHomeButtonEnabled(true);
+
 			drawerList = (ListView) findViewById(R.id.drawer_list);
 			navAdapter = new NavigationAdapter(this);
 			drawerList.setAdapter(navAdapter);
 			navAdapter.addSeperatorItem(getString(R.string.nav_hl_library));
-			navAdapter.addTextItemWithIcon(getString(R.string.nav_search), R.drawable.ic_action_search);
-			navAdapter.addTextItemWithIcon(getString(R.string.nav_account), R.drawable.ic_action_account);
-			navAdapter.addTextItemWithIcon(getString(R.string.nav_starred), R.drawable.ic_action_star_1);
-			navAdapter.addTextItemWithIcon(getString(R.string.nav_info), R.drawable.ic_action_info);
-			
+			navAdapter.addTextItemWithIcon(getString(R.string.nav_search),
+					R.drawable.ic_action_search);
+			navAdapter.addTextItemWithIcon(getString(R.string.nav_account),
+					R.drawable.ic_action_account);
+			navAdapter.addTextItemWithIcon(getString(R.string.nav_starred),
+					R.drawable.ic_action_star_1);
+			navAdapter.addTextItemWithIcon(getString(R.string.nav_info),
+					R.drawable.ic_action_info);
+
 			aData.open();
 			accounts = aData.getAllAccounts();
-			if (accounts.size() > 1) {	
-				navAdapter.addSeperatorItem(getString(R.string.nav_hl_accountlist));
-				
-				long tolerance = Long.decode(sp.getString("notification_warning",
-						"367200000"));
-				
+			if (accounts.size() > 1) {
+				navAdapter
+						.addSeperatorItem(getString(R.string.nav_hl_accountlist));
+
+				long tolerance = Long.decode(sp.getString(
+						"notification_warning", "367200000"));
+
 				for (final Account account : accounts) {
 					Library library;
 					try {
@@ -162,29 +170,33 @@ public abstract class OpacActivity extends Activity {
 						if (expiring > 0) {
 							expiringText = String.valueOf(expiring);
 						}
-						navAdapter.addLibraryItem(account.getLabel(), library.getCity(), expiringText);
-						
+						navAdapter.addLibraryItem(account.getLabel(),
+								library.getCity(), expiringText);
+
 					} catch (IOException e) {
 						e.printStackTrace();
 					} catch (JSONException e) {
 						e.printStackTrace();
 					}
 				}
-				selectItem(5); //selects first account
+				selectItem(5); // selects first account
 			}
-			
+
 			navAdapter.addSeperatorItem(getString(R.string.nav_hl_other));
-			navAdapter.addTextItemWithIcon(getString(R.string.nav_settings), R.drawable.ic_action_settings);
-			navAdapter.addTextItemWithIcon(getString(R.string.nav_about), R.drawable.ic_action_help);
-			
-			drawerList.setOnItemClickListener(new DrawerItemClickListener());		
-			
+			navAdapter.addTextItemWithIcon(getString(R.string.nav_settings),
+					R.drawable.ic_action_settings);
+			navAdapter.addTextItemWithIcon(getString(R.string.nav_about),
+					R.drawable.ic_action_help);
+
+			drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
 			if (!sp.getBoolean("version2.0.0-introduced", false)
 					&& app.getSlidingMenuEnabled()) {
 				final Handler handler = new Handler();
 				// Just show the menu to explain that is there if people start
 				// version 2 for the first time.
-				// We need a handler because if we just put this in onCreate nothing
+				// We need a handler because if we just put this in onCreate
+				// nothing
 				// happens. I don't have any idea, why.
 				handler.postDelayed(new Runnable() {
 					@Override
@@ -196,79 +208,83 @@ public abstract class OpacActivity extends Activity {
 								.commit();
 					}
 				}, 500);
-	
+
 			}
 		}
 	}
-	
+
 	private class DrawerItemClickListener implements OnItemClickListener {
 		@Override
-		public void onItemClick(AdapterView parent, View view, int position, long id) {
+		public void onItemClick(AdapterView parent, View view, int position,
+				long id) {
 			selectItem(position);
-		}		
+		}
 	}
-	
-	 @Override
-	    protected void onPostCreate(Bundle savedInstanceState) {
-	        super.onPostCreate(savedInstanceState);
-	        if(hasDrawer) drawerToggle.syncState();
-	    }
 
-	    @Override
-	    public void onConfigurationChanged(Configuration newConfig) {
-	        super.onConfigurationChanged(newConfig);
-	        if(hasDrawer) drawerToggle.onConfigurationChanged(newConfig);
-	    }
-	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		if (hasDrawer)
+			drawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		if (hasDrawer)
+			drawerToggle.onConfigurationChanged(newConfig);
+	}
+
 	/** Swaps fragments in the main content view */
 	protected void selectItem(int position) {
-	    final int count = navAdapter.getCount();
-	    if (navAdapter.getItemViewType(position) == Item.TYPE_SEPARATOR) {
-	    	//clicked on a separator
-	    	return;
-	    } else if (navAdapter.getItemViewType(position) == Item.TYPE_TEXT) {
-	    	switch (position) {
-	    		case 1: fragment = new SearchFragment();
-	    				break;
-	    		case 2: //fragment = new AccountFragment();
-	    				break;
-	    		case 3: //fragment = new StarredFragment();
-    				break;
-	    		case 4: fragment = new InfoFragment();
-    				break;
-	    	}
-			if(position == count - 2) {
+		final int count = navAdapter.getCount();
+		if (navAdapter.getItemViewType(position) == Item.TYPE_SEPARATOR) {
+			// clicked on a separator
+			return;
+		} else if (navAdapter.getItemViewType(position) == Item.TYPE_TEXT) {
+			switch (position) {
+			case 1:
+				fragment = new SearchFragment();
+				break;
+			case 2: // fragment = new AccountFragment();
+				break;
+			case 3: // fragment = new StarredFragment();
+				break;
+			case 4:
+				fragment = new InfoFragment();
+				break;
+			}
+			if (position == count - 2) {
 				Intent intent = new Intent(this, MainPreferenceActivity.class);
 				startActivity(intent);
 				return;
 			} else if (position == count - 1) {
-				//fragment = new AboutFragment();
+				// fragment = new AboutFragment();
 			}
-			
 
-		    // Insert the fragment by replacing any existing fragment
-		    FragmentManager fragmentManager = getSupportFragmentManager();
-		    fragmentManager.beginTransaction()
-		                   .replace(R.id.content_frame, fragment)
-		                   .commit();
+			// Insert the fragment by replacing any existing fragment
+			FragmentManager fragmentManager = getSupportFragmentManager();
+			fragmentManager.beginTransaction()
+					.replace(R.id.content_frame, fragment).commit();
 
-		    // Highlight the selected item, update the title, and close the drawer
-		    deselectNavItems();
-		    drawerList.setItemChecked(position, true);
-		    drawerList.setItemChecked(selectedItemPos, false);
-		    selectedItemPos = position;
-		    setTitle(navAdapter.getItem(position).text);
-		    drawerLayout.closeDrawer(drawerList);
-			
+			// Highlight the selected item, update the title, and close the
+			// drawer
+			deselectNavItems();
+			drawerList.setItemChecked(position, true);
+			drawerList.setItemChecked(selectedItemPos, false);
+			selectedItemPos = position;
+			setTitle(navAdapter.getItem(position).text);
+			drawerLayout.closeDrawer(drawerList);
+
 		} else if (navAdapter.getItemViewType(position) == Item.TYPE_LIBRARY) {
 			deselectLibraryItems();
 			drawerList.setItemChecked(position, true);
-			selectaccount(accounts.get(position-6).getId());
-		    drawerLayout.closeDrawer(drawerList);
+			selectaccount(accounts.get(position - 6).getId());
+			drawerLayout.closeDrawer(drawerList);
 			return;
 		}
 	}
-	
+
 	@Override
 	public void setTitle(CharSequence title) {
 		super.setTitle(title);
@@ -276,16 +292,16 @@ public abstract class OpacActivity extends Activity {
 	}
 
 	private void deselectLibraryItems() {
-		for(int i = 6; i < drawerList.getCount() - 2; i++) {
+		for (int i = 6; i < drawerList.getCount() - 2; i++) {
 			drawerList.setItemChecked(i, false);
 		}
 	}
-	
+
 	private void deselectNavItems() {
-		for(int i = 1; i < 5; i++) {
+		for (int i = 1; i < 5; i++) {
 			drawerList.setItemChecked(i, false);
 		}
-		for(int i = drawerList.getCount() - 2; i < drawerList.getCount(); i++) {
+		for (int i = drawerList.getCount() - 2; i < drawerList.getCount(); i++) {
 			drawerList.setItemChecked(i, false);
 		}
 	}
@@ -323,7 +339,7 @@ public abstract class OpacActivity extends Activity {
 	@Override
 	protected void onStop() {
 		super.onStop();
-//		showContent();
+		// showContent();
 	}
 
 	@Override
@@ -434,7 +450,8 @@ public abstract class OpacActivity extends Activity {
 
 				adialog.dismiss();
 
-				((AccountSelectedListener) fragment).accountSelected(accounts.get(position));
+				((AccountSelectedListener) fragment).accountSelected(accounts
+						.get(position));
 			}
 		});
 		builder.setTitle(R.string.account_select)
@@ -451,15 +468,16 @@ public abstract class OpacActivity extends Activity {
 							@Override
 							public void onClick(DialogInterface dialog, int id) {
 								dialog.dismiss();
-//TODO:								Intent intent = new Intent(OpacActivity.this,
-//										AccountListActivity.class);
-//								startActivity(intent);
+								// TODO: Intent intent = new
+								// Intent(OpacActivity.this,
+								// AccountListActivity.class);
+								// startActivity(intent);
 							}
 						});
 		adialog = builder.create();
 		adialog.show();
 	}
-	
+
 	public void selectaccount(long id) {
 		((OpacClient) getApplication()).setAccount(id);
 		accountSelected(((OpacClient) getApplication()).getAccount());
@@ -484,11 +502,11 @@ public abstract class OpacActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (hasDrawer && drawerToggle.onOptionsItemSelected(item)) {
-          return true;
-        }
-        
-        return super.onOptionsItemSelected(item);
+		// true, then it has handled the app icon touch event
+		if (hasDrawer && drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
