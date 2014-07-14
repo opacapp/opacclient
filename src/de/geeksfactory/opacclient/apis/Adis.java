ff014842fa4a79c29dc1aa617d53e2c9dd6df8f0
Raphael Michel <mail@raphaelmichel.de>
2014-07-14 21:28:28 +0200

    Fix aDIS selection

--- src/de/geeksfactory/opacclient/apis/Adis.java
+++ src/de/geeksfactory/opacclient/apis/Adis.java
@@ -680,6 +680,9 @@ public class Adis extends BaseApi implements OpacApi {
 		List<NameValuePair> nvpairs;
 		ReservationResult res = null;
 
+		if(selection.equals(""))
+			selection = null;
+
 		if (s_pageform == null)
 			return new ReservationResult(Status.ERROR);
 
