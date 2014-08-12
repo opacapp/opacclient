e26799babcb11523365bf70befd23a1e9d7025f7
johan12345 <johan.forstner@gmail.com>
2014-08-12 12:13:39 +0200

    Make Unit tests work with WinBiap

--- src/de/geeksfactory/opacclient/apis/WinBiap.java
+++ src/de/geeksfactory/opacclient/apis/WinBiap.java
@@ -1,7 +1,6 @@
 package de.geeksfactory.opacclient.apis;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
