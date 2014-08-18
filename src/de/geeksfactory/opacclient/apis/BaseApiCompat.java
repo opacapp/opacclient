907df3d7f6241feda5424032b58dc9366a02da60
johan12345 <johan.forstner@gmail.com>
2014-08-18 13:01:12 +0200

    Sort search fields correctly in BaseApiCompat implementation

--- src/de/geeksfactory/opacclient/apis/BaseApiCompat.java
+++ src/de/geeksfactory/opacclient/apis/BaseApiCompat.java
@@ -22,38 +22,40 @@ public abstract class BaseApiCompat extends BaseApi implements OpacApi {
 				searchFields.add(new TextSearchField(field, "Titel", false, false, "Stichwort", false, false));
 			} else if (field.equals(KEY_SEARCH_QUERY_AUTHOR)) {
 				searchFields.add(new TextSearchField(field, "Verfasser", false, false, "Nachname, Vorname", false, false));
-			} else if (field.equals(KEY_SEARCH_QUERY_KEYWORDA)) {
-				searchFields.add(new TextSearchField(field, "Schlagwort", true, false, "", false, false));
-			} else if (field.equals(KEY_SEARCH_QUERY_KEYWORDB)) {
-				searchFields.add(new TextSearchField(field, "Schlagwort", true, true, "", false, false));
-			} else if (field.equals(KEY_SEARCH_QUERY_BRANCH)) {
-				//TODO:
-			} else if (field.equals(KEY_SEARCH_QUERY_HOME_BRANCH)) {
-				//TODO:
+			} else if (field.equals(KEY_SEARCH_QUERY_DIGITAL)) {
+				searchFields.add(new CheckboxSearchField(field, "nur digitale Medien", false));
+			} else if (field.equals(KEY_SEARCH_QUERY_AVAILABLE)) {
+				searchFields.add(new CheckboxSearchField(field, "nur verfügbare Medien", false));
 			} else if (field.equals(KEY_SEARCH_QUERY_ISBN)) {
 				searchFields.add(new BarcodeSearchField(field, "Strichcode", false, false, "ISBN"));
+			} else if (field.equals(KEY_SEARCH_QUERY_BARCODE)) {
+				searchFields.add(new BarcodeSearchField(field, "Strichcode", false, false, "Buchungsnr."));
 			} else if (field.equals(KEY_SEARCH_QUERY_YEAR)) {
 				searchFields.add(new TextSearchField(field, "Jahr", false, false, "", false, true));
 			} else if (field.equals(KEY_SEARCH_QUERY_YEAR_RANGE_START)) {
 				searchFields.add(new TextSearchField(field, "Jahr", false, false, "von", false, true));
 			} else if (field.equals(KEY_SEARCH_QUERY_YEAR_RANGE_END)) {
 				searchFields.add(new TextSearchField(field, "Jahr", false, true, "bis", false, true));
+			} else if (field.equals(KEY_SEARCH_QUERY_BRANCH)) {
+				//TODO:
+			} else if (field.equals(KEY_SEARCH_QUERY_HOME_BRANCH)) {
+				//TODO:	
+			} else if (field.equals(KEY_SEARCH_QUERY_CATEGORY)) {
+				//TODO:
+			} else if (field.equals(KEY_SEARCH_QUERY_PUBLISHER)) {
+				searchFields.add(new TextSearchField(field, "Verlag", false, false, "", false, false));
+			} else if (field.equals(KEY_SEARCH_QUERY_KEYWORDA)) {
+				searchFields.add(new TextSearchField(field, "Schlagwort", true, false, "", false, false));
+			} else if (field.equals(KEY_SEARCH_QUERY_KEYWORDB)) {
+				searchFields.add(new TextSearchField(field, "Schlagwort", true, true, "", false, false));
 			} else if (field.equals(KEY_SEARCH_QUERY_SYSTEM)) {
 				searchFields.add(new TextSearchField(field, "Systematik", true, false, "", false, false));
 			} else if (field.equals(KEY_SEARCH_QUERY_AUDIENCE)) {
-				searchFields.add(new TextSearchField(field, "Interessenkreis", true, false, "", false, false));
-			} else if (field.equals(KEY_SEARCH_QUERY_PUBLISHER)) {
-				searchFields.add(new TextSearchField(field, "Verlag", false, false, "", false, false));
-			} else if (field.equals(KEY_SEARCH_QUERY_CATEGORY)) {
-				//TODO:
-			} else if (field.equals(KEY_SEARCH_QUERY_BARCODE)) {
-				searchFields.add(new BarcodeSearchField(field, "Strichcode", false, false, "Buchungsnr."));
+				searchFields.add(new TextSearchField(field, "Interessenkreis", true, false, "", false, false));		
 			} else if (field.equals(KEY_SEARCH_QUERY_LOCATION)) {
 				searchFields.add(new TextSearchField(field, "Ort", false, false, "", false, false));
-			} else if (field.equals(KEY_SEARCH_QUERY_DIGITAL)) {
-				searchFields.add(new CheckboxSearchField(field, "nur digitale Medien", false));
-			} else if (field.equals(KEY_SEARCH_QUERY_AVAILABLE)) {
-				searchFields.add(new CheckboxSearchField(field, "nur verfügbare Medien", false));
+			} else if (field.equals(KEY_SEARCH_QUERY_ORDER)) {
+				//TODO:
 			}
 		}
 		return searchFields;
