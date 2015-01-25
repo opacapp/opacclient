54cca580cfd7a35215693e56e6f4ed7c6fd689ce
Johan von Forstner <johan.forstner@gmail.com>
2015-01-25 14:14:55 +0100

    Make PreferenceActivity and AboutActivity work with AppCompat

--- opacclient/src/main/java/de/geeksfactory/opacclient/frontend/AboutActivity.java
+++ opacclient/src/main/java/de/geeksfactory/opacclient/frontend/AboutActivity.java
@@ -33,6 +33,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
 import android.text.Html;
 import android.util.Log;
 import android.view.MenuItem;
@@ -43,198 +44,14 @@ import android.widget.TextView;
 import de.geeksfactory.opacclient.OpacClient;
 import de.geeksfactory.opacclient.R;
 
-public class AboutActivity extends OpacPreferenceActivity {
+public class AboutActivity extends ActionBarActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-		populate();
-	}
-
-	@SuppressWarnings("deprecation")
-	protected void populate() {
-		addPreferencesFromResource(R.xml.about);
-
-		String version = OpacClient.versionName;
-
-		try {
-			String text = "";
-
-			StringBuilder builder = new StringBuilder();
-			InputStream fis;
-			fis = getAssets().open("buildnum.txt");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					fis, "utf-8"));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-			}
-
-			text = builder.toString();
-			fis.close();
-			if (!text.equals(version))
-				version += " (Build: " + text + ")";
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		findPreference("version").setSummary(version);
-
-		findPreference("website").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference preference) {
-						Intent i = new Intent(Intent.ACTION_VIEW);
-						if (getString(R.string.website_url).contains("de"))
-							i.setData(Uri.parse("http://de.opacapp.net"));
-						else
-							i.setData(Uri.parse("http://en.opacapp.net"));
-						startActivity(i);
-						return false;
-					}
-				});
-
-		findPreference("developer").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference preference) {
-						Intent i = new Intent(Intent.ACTION_VIEW);
-						i.setData(Uri.parse("http://www.raphaelmichel.de"));
-						startActivity(i);
-						return false;
-					}
-				});
-
-		findPreference("feedback").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference preference) {
-						Intent emailIntent = new Intent(
-								android.content.Intent.ACTION_SEND);
-						emailIntent.putExtra(
-								android.content.Intent.EXTRA_EMAIL,
-								new String[] { "info@opacapp.de" });
-						emailIntent.setType("text/plain");
-						startActivity(Intent.createChooser(emailIntent,
-								getString(R.string.write_mail)));
-						return false;
-					}
-				});
-
-		findPreference("rate_play").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference preference) {
-						try {
-							Intent i = new Intent(
-									Intent.ACTION_VIEW,
-									Uri.parse("market://details?id=de.geeksfactory.opacclient"));
-							startActivity(i);
-						} catch (ActivityNotFoundException e) {
-							Log.i("rate_play", "no market installed");
-						}
-						return false;
-					}
-				});
-
-		findPreference("source").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference preference) {
-						Intent i = new Intent(Intent.ACTION_VIEW, Uri
-								.parse("http://github.com/raphaelm/opacclient"));
-						startActivity(i);
-						return false;
-					}
-				});
-
-		findPreference("osl").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference preference) {
-						res_dialog(R.raw.licenses, R.string.osl);
-
-						return false;
-					}
-				});
-
-		findPreference("privacy").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference preference) {
-						res_dialog(R.raw.privacy, R.string.privacy);
-						return false;
-					}
-				});
-
-		findPreference("thanks").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference preference) {
-						res_dialog(R.raw.thanks, R.string.thanks);
-						return false;
-					}
-				});
-
-		findPreference("changelog").setOnPreferenceClickListener(
-				new Preference.OnPreferenceClickListener() {
-					@Override
-					public boolean onPreferenceClick(Preference arg0) {
-						res_dialog(R.raw.changelog, R.string.changelog);
-						return false;
-					}
-				});
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	private void res_dialog(int file, int title) {
-
-		final Dialog dialog = new Dialog(AboutActivity.this);
-		dialog.setContentView(R.layout.dialog_about);
-		dialog.setTitle(title);
-		TextView textview1 = (TextView) dialog.findViewById(R.id.textView1);
-
-		String text = "";
-
-		StringBuilder builder = new StringBuilder();
-		InputStream fis;
-		try {
-			fis = getResources().openRawResource(file);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					fis, "utf-8"));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-			}
-
-			text = builder.toString();
-			fis.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		textview1.setText(Html.fromHtml(text));
-
-		Button dialogButton = (Button) dialog.findViewById(R.id.button1);
-		// if button is clicked, close the custom dialog
-		dialogButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				dialog.dismiss();
-			}
-		});
-
-		dialog.show();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new AboutFragment())
+                .commit();
+    }
 }
--- opacclient/src/main/java/de/geeksfactory/opacclient/frontend/MainPreferenceActivity.java
+++ opacclient/src/main/java/de/geeksfactory/opacclient/frontend/MainPreferenceActivity.java
@@ -1,102 +1,17 @@
-/**
- * Copyright (C) 2013 by Raphael Michel under the MIT license:
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the Software 
- * is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
- * DEALINGS IN THE SOFTWARE.
- */
 package de.geeksfactory.opacclient.frontend;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.view.MenuItem;
-import de.geeksfactory.opacclient.OpacClient;
+import android.support.v7.app.ActionBarActivity;
+
 import de.geeksfactory.opacclient.R;
-import de.geeksfactory.opacclient.reminder.ReminderCheckService;
-import de.geeksfactory.opacclient.storage.AccountDataSource;
-import de.geeksfactory.opacclient.storage.JsonSearchFieldDataSource;
-import de.geeksfactory.opacclient.storage.SearchFieldDataSource;
 
-public class MainPreferenceActivity extends OpacPreferenceActivity {
-
-	protected boolean ebooksSupported() {
-		return false;
-	}
-
-	@Override
-	@SuppressWarnings("deprecation")
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-		addPreferencesFromResource(R.xml.settings);
-
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH
-				|| !getPackageManager()
-						.hasSystemFeature("android.hardware.nfc")) {
-			findPreference("nfc_search").setEnabled(false);
-		}
-
-		Preference assistant = findPreference("accounts");
-		assistant.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(Preference arg0) {
-				((OpacClient) getApplication())
-						.openAccountList(MainPreferenceActivity.this);
-				return false;
-			}
-		});
-
-		if (!ebooksSupported()) {
-			((PreferenceCategory) findPreference("cat_web_opac"))
-					.removePreference(findPreference("email"));
-		}
-
-		Preference meta = findPreference("meta_clear");
-		meta.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(Preference arg0) {
-				AccountDataSource adata = new AccountDataSource(
-						MainPreferenceActivity.this);
-				adata.open();
-				adata.invalidateCachedData();
-				adata.notificationClearCache(true);
-				adata.close();
-
-				SearchFieldDataSource sfdata = new JsonSearchFieldDataSource(
-						MainPreferenceActivity.this);
-				sfdata.clearAll();
-
-				Intent i = new Intent(MainPreferenceActivity.this,
-						ReminderCheckService.class);
-				startService(i);
-				return false;
-			}
-		});
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		}
-		return super.onOptionsItemSelected(item);
-	}
+public class MainPreferenceActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new MainPreferenceFragment())
+                .commit();
+    }
 }
