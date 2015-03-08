8f68e086e0593bdf0affb85992292cd7e9ce3015
Raphael Michel <mail@raphaelmichel.de>
2015-03-08 21:26:14 +0100

    Update toolchain

--- tools/geoloc.py
+++ tools/geoloc.py
@@ -8,41 +8,41 @@ import sys
 import json
 import os
 
-DIR = 'assets/bibs/'
-    
+DIR = 'opacclient/src/main/assets/bibs/'
+
 for filename in os.listdir(DIR):
 	f = DIR + filename
 	data = json.load(open(f))
 	if 'geo' in data:
 		continue
-		
+
 	print("Suche Position für: %s, %s\n" % (data['city'], data['group']))
-	
+
 	uri = 'https://maps.googleapis.com/maps/api/geocode/json?'+urllib.parse.urlencode({'address':data['city'],'sensor':'false'})
 	jsoncontent = urllib.request.urlopen(uri).read().decode()
 	geocode = json.loads(jsoncontent)
-	
+
 	if geocode['status'] != 'OK':
 		print("ERROR! %s" %filename)
 		continue
-		
+
 	key = 1
 	for res in geocode['results']:
 		print("["+str(key)+"]", ", ".join([a["long_name"] for a in res['address_components']]))
 		key += 1
-		
+
 	print("\nWelches? [Enter für ablehnen]")
-		
+
 	inp = input()
-	
+
 	os.system('clear')
-	
+
 	if inp.strip() == '':
 		continue
-		
+
 	if int(inp.strip()) > 0:
 		res = geocode['results'][int(inp.strip())-1]
 		data['geo'] = [float(res['geometry']['location']['lat']), float(res['geometry']['location']['lng'])]
-	
+
 
 	json.dump(data, open(f, 'w'), indent=4, sort_keys=True)
