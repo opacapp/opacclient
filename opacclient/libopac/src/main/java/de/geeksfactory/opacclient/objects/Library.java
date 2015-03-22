02c3e22637a06783cab87c0cf054869a12a135b1
Raphael Michel <mail@raphaelmichel.de>
2015-03-22 15:35:12 +0100

    Remove the previously introduced @NotNull, as it might cause problems on android (#281)

--- opacclient/libopac/src/main/java/de/geeksfactory/opacclient/objects/Library.java
+++ opacclient/libopac/src/main/java/de/geeksfactory/opacclient/objects/Library.java
@@ -21,8 +21,6 @@
  */
 package de.geeksfactory.opacclient.objects;
 
-import com.sun.istack.internal.NotNull;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -293,7 +291,7 @@ public class Library implements Comparable<Library> {
     }
 
     @Override
-    public int compareTo(@NotNull Library arg0) {
+    public int compareTo(Library arg0) {
         Collator deCollator = Collator.getInstance(Locale.GERMAN);
         deCollator.setStrength(Collator.TERTIARY);
 
