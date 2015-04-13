99cd00a5a4060615d078d8a95abcb790017fe546
johan12345 <johan.forstner@gmail.com>
2015-04-13 22:36:34 +0200

    Start adding animations to ExpandingCardListManager

--- opacclient/opacapp/src/main/java/de/geeksfactory/opacclient/ui/ExpandingCardListManager.java
+++ opacclient/opacapp/src/main/java/de/geeksfactory/opacclient/ui/ExpandingCardListManager.java
@@ -3,9 +3,19 @@ package de.geeksfactory.opacclient.ui;
 import android.content.Context;
 import android.support.v7.internal.widget.TintManager;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
+
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.ValueAnimator;
+import com.nineoldandroids.view.ViewHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.geeksfactory.opacclient.R;
 
@@ -24,6 +34,7 @@ public abstract class ExpandingCardListManager {
     private LinearLayout llLower;
     private CardView expandedCard;
     private TintManager tint;
+    private List<View> views = new ArrayList<>();
 
     public ExpandingCardListManager (Context context, LinearLayout layout) {
         this.context = context;
@@ -49,6 +60,7 @@ public abstract class ExpandingCardListManager {
 
         CardView.LayoutParams listParams = new CardView.LayoutParams(CardView.LayoutParams.MATCH_PARENT, CardView.LayoutParams.WRAP_CONTENT);
 
+        // Upper card
         upperCard = new CardView(context);
         upperCard.setVisibility(View.GONE);
         upperCard.setLayoutParams(cardParams);
@@ -60,22 +72,13 @@ public abstract class ExpandingCardListManager {
         upperCard.addView(llUpper);
         layout.addView(upperCard);
 
-        mainCard = new CardView(context);
-        mainCard.setLayoutParams(cardParams);
-
-        llMain = new LinearLayout(context);
-        llMain.setLayoutParams(listParams);
-        llMain.setOrientation(LinearLayout.VERTICAL);
-
-        mainCard.addView(llMain);
-        layout.addView(mainCard);
-
+        // Expanded card
         expandedCard = new CardView(context);
         expandedCard.setVisibility(View.GONE);
         expandedCard.setLayoutParams(expandedCardParams);
-        expandedCard.setCardElevation(context.getResources().getDimension(R.dimen.card_elevation_selected));
         layout.addView(expandedCard);
 
+        // Lower card
         lowerCard = new CardView(context);
         lowerCard.setVisibility(View.GONE);
         lowerCard.setLayoutParams(cardParams);
@@ -86,11 +89,24 @@ public abstract class ExpandingCardListManager {
 
         lowerCard.addView(llLower);
         layout.addView(lowerCard);
+
+        // Main card
+        mainCard = new CardView(context);
+        mainCard.setLayoutParams(cardParams);
+
+        llMain = new LinearLayout(context);
+        llMain.setLayoutParams(listParams);
+        llMain.setOrientation(LinearLayout.VERTICAL);
+
+        mainCard.addView(llMain);
+        layout.addView(mainCard);
     }
 
     private void addViews() {
         for (int i = 0; i < getCount(); i++) {
-            llMain.addView(getView(i, llMain));
+            View view = getView(i, llMain);
+            views.add(view);
+            llMain.addView(view);
             if (i < getCount() - 1) llMain.addView(createDivider());
         }
     }
@@ -98,7 +114,7 @@ public abstract class ExpandingCardListManager {
     private View createDivider() {
         View divider = new View(context);
         divider.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int) context.getResources().getDisplayMetrics().density));
-        divider.setBackground(tint.getDrawable( R.drawable.abc_list_divider_mtrl_alpha));
+        divider.setBackground(tint.getDrawable(R.drawable.abc_list_divider_mtrl_alpha));
         return divider;
     }
 
@@ -112,7 +128,7 @@ public abstract class ExpandingCardListManager {
             llUpper.addView(getView(i, llUpper));
             if (i < position - 1) llUpper.addView(createDivider());
         }
-        View expandedView = getView(position, expandedCard);
+        final View expandedView = getView(position, expandedCard);
         expandView(position, expandedView);
         expandedCard.addView(expandedView);
         for (int i = position + 1; i < getCount(); i++) {
@@ -120,10 +136,44 @@ public abstract class ExpandingCardListManager {
             if (i < getCount() - 1) llLower.addView(createDivider());
         }
 
-        mainCard.setVisibility(View.GONE);
+        final float lowerPos;
+        if (position + 1 < getCount()) lowerPos = ViewHelper.getY(views.get(position + 1));
+        else lowerPos = -1;
+
+        final float mainPos = ViewHelper.getY(views.get(position));
+        final int unexpandedHeight = views.get(position).getHeight();
+
         upperCard.setVisibility(View.VISIBLE);
         lowerCard.setVisibility(View.VISIBLE);
         expandedCard.setVisibility(View.VISIBLE);
+        mainCard.setVisibility(View.GONE);
+
+        layout.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                layout.getViewTreeObserver().removeOnPreDrawListener(this);
+                if (lowerPos > 0) ViewHelper.setY(lowerCard, lowerPos - lowerCard.getPaddingTop());
+                ViewHelper.setY(expandedCard, mainPos - expandedCard.getPaddingTop());
+
+                AnimatorSet set = new AnimatorSet();
+                int defaultMargin = context.getResources().getDimensionPixelSize(
+                        R.dimen.card_side_margin_default);
+                int expandedMargin = context.getResources().getDimensionPixelSize(
+                        R.dimen.card_side_margin_selected);
+                int marginDifference = expandedMargin - defaultMargin;
+                set.playTogether(
+                        ObjectAnimator.ofFloat(lowerCard, "translationY", lowerCard.getTranslationY(), 0),
+                        ObjectAnimator.ofFloat(expandedCard, "translationY", expandedCard.getTranslationY(), 0),
+                        ObjectAnimator.ofFloat(expandedCard, "cardElevation", context.getResources().getDimension(R.dimen.card_elevation_default),
+                                context.getResources().getDimension(R.dimen.card_elevation_selected)),
+                        ObjectAnimator.ofInt(expandedCard, "bottom", expandedCard.getBottom() + unexpandedHeight - expandedView.getHeight(), expandedCard.getBottom()),
+                        ObjectAnimator.ofInt(expandedCard, "left", expandedCard.getLeft() - marginDifference, expandedCard.getLeft()),
+                        ObjectAnimator.ofInt(expandedCard, "right", expandedCard.getRight() + marginDifference, expandedCard.getRight())
+                );
+                set.setDuration(500).start();
+                return false;
+            }
+        });
 
         expandedPosition = position;
     }
