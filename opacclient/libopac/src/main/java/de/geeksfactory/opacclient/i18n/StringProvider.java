ac2fb88bc5ac712e8efc65f580437930cc04bb78
Johan von Forstner <johan.forstner@gmail.com>
2015-09-02 10:06:08 +0200

    StringProvider: Remove unnecessary "public static" and "public abstract" modifiers

--- opacclient/libopac/src/main/java/de/geeksfactory/opacclient/i18n/StringProvider.java
+++ opacclient/libopac/src/main/java/de/geeksfactory/opacclient/i18n/StringProvider.java
@@ -36,45 +36,45 @@ package de.geeksfactory.opacclient.i18n;
  * @since 3.2.1
  */
 public interface StringProvider {
-    public static String LIMITED_NUM_OF_CRITERIA = "limited_num_of_criteria";
-    public static String NO_CRITERIA_INPUT = "no_criteria_input";
-    public static String COMBINATION_NOT_SUPPORTED = "combination_not_supported";
-    public static String UNKNOWN_ERROR = "unknown_error";
-    public static String UNKNOWN_ERROR_WITH_DESCRIPTION = "unknown_error_with_description";
-    public static String UNKNOWN_ERROR_ACCOUNT = "unknown_error_account";
-    public static String UNKNOWN_ERROR_ACCOUNT_WITH_DESCRIPTION =
+    String LIMITED_NUM_OF_CRITERIA = "limited_num_of_criteria";
+    String NO_CRITERIA_INPUT = "no_criteria_input";
+    String COMBINATION_NOT_SUPPORTED = "combination_not_supported";
+    String UNKNOWN_ERROR = "unknown_error";
+    String UNKNOWN_ERROR_WITH_DESCRIPTION = "unknown_error_with_description";
+    String UNKNOWN_ERROR_ACCOUNT = "unknown_error_account";
+    String UNKNOWN_ERROR_ACCOUNT_WITH_DESCRIPTION =
             "unknown_error_account_with_description";
-    public static String INTERNAL_ERROR = "internal_error";
-    public static String INTERNAL_ERROR_WITH_DESCRIPTION = "internal_error_with_description";
-    public static String LOGIN_FAILED = "login_failed";
-    public static String COULD_NOT_LOAD_ACCOUNT = "could_not_load_account";
-    public static String CONNECTION_ERROR = "api_connection_error";
-    public static String LENT_UNTIL = "lent_until";
-    public static String SUBTITLE = "subtitle";
-    public static String PICA_WHICH_COPY = "pica_which_copy";
-    public static String NO_COPY_RESERVABLE = "no_copy_reservable";
-    public static String COULD_NOT_LOAD_DETAIL = "could_not_load_detail";
-    public static String ERROR = "error";
-    public static String DOWNLOAD = "download";
-    public static String REMINDERS = "reminders";
-    public static String PROLONGED_ABBR = "prolonged_abbr";
-    public static String FREE_SEARCH = "free_search";
-    public static String WRONG_LOGIN_DATA = "wrong_account_data";
-    public static String PROLONGING_IMPOSSIBLE = "prolonging_impossible";
-    public static String PROLONGING_EXPIRED = "prolonging_expired";
-    public static String PROLONGING_WAITING = "prolonging_expired";
-    public static String ORDER = "order";
-    public static String ORDER_DEFAULT = "order_default";
-    public static String ORDER_YEAR_ASC = "order_year_asc";
-    public static String ORDER_CATEGORY_ASC = "order_category_asc";
-    public static String ORDER_YEAR_DESC = "order_year_desc";
-    public static String ORDER_CATEGORY_DESC = "order_category_desc";
-    public static String NO_RESULTS = "no_results";
-    public static String INTERLIB_BRANCH = "interlib_branch";
-    public static String STACKS_BRANCH = "stacks_branch";
-    public static String PROVISION_BRANCH = "provision_branch";
-    public static String UNSUPPORTED_IN_LIBRARY = "unsupported_in_library";
-    public static String RESERVATION_WARNING = "reservation_warning";
+    String INTERNAL_ERROR = "internal_error";
+    String INTERNAL_ERROR_WITH_DESCRIPTION = "internal_error_with_description";
+    String LOGIN_FAILED = "login_failed";
+    String COULD_NOT_LOAD_ACCOUNT = "could_not_load_account";
+    String CONNECTION_ERROR = "api_connection_error";
+    String LENT_UNTIL = "lent_until";
+    String SUBTITLE = "subtitle";
+    String PICA_WHICH_COPY = "pica_which_copy";
+    String NO_COPY_RESERVABLE = "no_copy_reservable";
+    String COULD_NOT_LOAD_DETAIL = "could_not_load_detail";
+    String ERROR = "error";
+    String DOWNLOAD = "download";
+    String REMINDERS = "reminders";
+    String PROLONGED_ABBR = "prolonged_abbr";
+    String FREE_SEARCH = "free_search";
+    String WRONG_LOGIN_DATA = "wrong_account_data";
+    String PROLONGING_IMPOSSIBLE = "prolonging_impossible";
+    String PROLONGING_EXPIRED = "prolonging_expired";
+    String PROLONGING_WAITING = "prolonging_expired";
+    String ORDER = "order";
+    String ORDER_DEFAULT = "order_default";
+    String ORDER_YEAR_ASC = "order_year_asc";
+    String ORDER_CATEGORY_ASC = "order_category_asc";
+    String ORDER_YEAR_DESC = "order_year_desc";
+    String ORDER_CATEGORY_DESC = "order_category_desc";
+    String NO_RESULTS = "no_results";
+    String INTERLIB_BRANCH = "interlib_branch";
+    String STACKS_BRANCH = "stacks_branch";
+    String PROVISION_BRANCH = "provision_branch";
+    String UNSUPPORTED_IN_LIBRARY = "unsupported_in_library";
+    String RESERVATION_WARNING = "reservation_warning";
 
     /**
      * Returns the translated string identified by identifier
@@ -82,7 +82,7 @@ public interface StringProvider {
      * @param identifier The ID of the string
      * @return the translated string
      */
-    public abstract String getString(String identifier);
+    String getString(String identifier);
 
     /**
      * Returns a translated formatted string
@@ -91,7 +91,7 @@ public interface StringProvider {
      * @param args       Formatting arguments
      * @return the translated and formatted string
      */
-    public abstract String getFormattedString(String identifier, Object... args);
+    String getFormattedString(String identifier, Object... args);
 
     /**
      * Returns a translated quantity string
@@ -101,5 +101,7 @@ public interface StringProvider {
      * @param args       Formatting arguments
      * @return the translated and formatted string
      */
-    public abstract String getQuantityString(String identifier, int count, Object... args);
+    String getQuantityString(String identifier, int count, Object... args);
+
+
 }
