1aff118a80c6f20e447db300e039897cbad2d41e
Simon Legner <Simon.Legner@gmail.com>
2015-09-17 15:11:18 +0200

    WebOpacAtTest: remove calls to online API

--- opacclient/tests/src/main/java/de/geeksfactory/opacclient/apis/WebOpacAtTest.java
+++ opacclient/tests/src/main/java/de/geeksfactory/opacclient/apis/WebOpacAtTest.java
@@ -1,23 +1,10 @@
 package de.geeksfactory.opacclient.apis;
 
-import org.json.JSONObject;
 import org.junit.Test;
 
-import java.util.Collections;
 import java.util.Objects;
 
-import de.geeksfactory.opacclient.objects.Library;
-import de.geeksfactory.opacclient.searchfields.SearchQuery;
-
 public class WebOpacAtTest {
-
-    WebOpacAt getApi() {
-        WebOpacAt api = new WebOpacAt();
-        Library lib = new Library();
-        lib.setData(new JSONObject(Collections.singletonMap("baseurl", "http://www.stbinnsbruck.web-opac.at")));
-        api.init(lib);
-        return api;
-    }
 
     @Test
     public void testParseTotalResults() {
@@ -30,16 +17,6 @@ public class WebOpacAtTest {
     public void testParseCopyReturn() throws Exception {
         assert Objects.equals(WebOpacAt.parseCopyReturn("1 (voraussichtl. bis 31.07.2015)"), "31.07.2015");
 
-    }
-
-    @Test
-    public void testSearchFields() throws Exception {
-        getApi().getSearchFields();
-    }
-
-    @Test
-    public void testEmptyQuery() throws Exception {
-        getApi().search(Collections.<SearchQuery>emptyList());
     }
 
 }
