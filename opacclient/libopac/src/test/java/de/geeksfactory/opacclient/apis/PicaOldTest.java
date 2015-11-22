223b89c3b782601c7dd3b25a5d252afd6ad4470b
Johan von Forstner <johan.forstner@gmail.com>
2015-11-22 20:49:39 +0100

    refactor PicaOld to allow using parseResList and parseMediaList in JUnit test

---
+++ opacclient/libopac/src/test/java/de/geeksfactory/opacclient/apis/PicaOldTest.java
@@ -0,0 +1,15 @@
+package de.geeksfactory.opacclient.apis;
+
+import org.junit.Test;
+
+public class PicaOldTest {
+    @Test
+    public void testParseMediaList() {
+
+    }
+
+    @Test
+    public void testParseResList() {
+
+    }
+}
