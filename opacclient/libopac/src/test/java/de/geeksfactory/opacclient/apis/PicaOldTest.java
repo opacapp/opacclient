77b66157472ad4422406c2e7482850066f519934
johan12345 <johan.forstner@gmail.com>
2015-11-30 20:41:41 +0100

    PicaOldTest: Add HTML sample from Frankfurt St. Georgen

--- opacclient/libopac/src/test/java/de/geeksfactory/opacclient/apis/PicaOldTest.java
+++ opacclient/libopac/src/test/java/de/geeksfactory/opacclient/apis/PicaOldTest.java
@@ -2,17 +2,39 @@ package de.geeksfactory.opacclient.apis;
 
 import org.jsoup.Jsoup;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import de.geeksfactory.opacclient.i18n.DummyStringProvider;
 
+@RunWith(Parameterized.class)
 public class PicaOldTest extends BaseTest {
+    private String file;
+
+    public PicaOldTest(String file) {
+        this.file = file;
+    }
+
+    private static final String[] FILES = new String[]{"marburg.html", "frankfurt_stgeorgen.html"};
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<String[]> files() {
+        List<String[]> files = new ArrayList<>();
+        for (String file : FILES) {
+            files.add(new String[]{file});
+        }
+        return files;
+    }
+
     @Test
     public void testParseMediaList() throws OpacApi.OpacErrorException {
-        String html = readResource("/pica_old/medialist/marburg.html");
+        String html = readResource("/pica_old/medialist/" + file);
+        if (html == null) return; // we may not have all files for all libraries
         List<Map<String, String>> media = new ArrayList<>();
         PicaOld.parseMediaList(media, Jsoup.parse(html), new DummyStringProvider(),
                 new ArrayList<String>());
@@ -20,7 +42,8 @@ public class PicaOldTest extends BaseTest {
 
     @Test
     public void testParseResList() throws OpacApi.OpacErrorException {
-        String html = readResource("/pica_old/reslist/marburg.html");
+        String html = readResource("/pica_old/reslist/" + file);
+        if (html == null) return; // we may not have all files for all libraries
         List<Map<String, String>> media = new ArrayList<>();
         PicaOld.parseResList(media, Jsoup.parse(html), new DummyStringProvider());
     }
