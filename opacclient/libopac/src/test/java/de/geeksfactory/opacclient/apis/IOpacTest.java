1315bac0d4ebbf8f8b365091df18683a4492e979
Johan von Forstner <johan.forstner@gmail.com>
2015-12-22 12:32:37 +0100

    Add IOpacTest using HTML sample from Heide

---
+++ opacclient/libopac/src/test/java/de/geeksfactory/opacclient/apis/IOpacTest.java
@@ -0,0 +1,53 @@
+package de.geeksfactory.opacclient.apis;
+
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import de.geeksfactory.opacclient.objects.AccountData;
+
+@RunWith(Parameterized.class)
+public class IOpacTest extends BaseTest {
+    private String file;
+
+    public IOpacTest(String file) {
+        this.file = file;
+    }
+
+    private static final String[] FILES = new String[]{"heide.html"};
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<String[]> files() {
+        List<String[]> files = new ArrayList<>();
+        for (String file : FILES) {
+            files.add(new String[]{file});
+        }
+        return files;
+    }
+
+    @Test
+    public void testParseMediaList() throws OpacApi.OpacErrorException {
+        String html = readResource("/iopac/" + file);
+        List<Map<String, String>> media = new ArrayList<>();
+        IOpac.parseMediaList(media, Jsoup.parse(html), new JSONObject());
+        for (Map<String, String> item : media) {
+            assertContainsData(item, AccountData.KEY_LENT_TITLE);
+            assertContainsData(item, AccountData.KEY_LENT_DEADLINE);
+            assertContainsData(item, AccountData.KEY_LENT_DEADLINE_TIMESTAMP);
+        }
+    }
+
+    @Test
+    public void testParseResList() throws OpacApi.OpacErrorException {
+        String html = readResource("/iopac/" + file);
+        List<Map<String, String>> media = new ArrayList<>();
+        IOpac.parseResList(media, Jsoup.parse(html), new JSONObject());
+    }
+}
