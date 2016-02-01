f81b536ea741eedc79591fcfa39183096a26002b
Raphael Michel <mail@raphaelmichel.de>
2016-02-01 20:36:52 +0100

    Added a celebration dialog for 1k libraries

---
+++ opacclient/opacapp/src/main/java/de/geeksfactory/opacclient/frontend/CelebrationDialogFragment.java
@@ -0,0 +1,49 @@
+package de.geeksfactory.opacclient.frontend;
+
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+
+import de.geeksfactory.opacclient.R;
+
+public class CelebrationDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_celeb, null))
+                // Add action buttons
+                .setNeutralButton(R.string.celeb_rate, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Uri uri = Uri.parse("market://details?id=de.geeksfactory.opacclient");
+                        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                        // To count with Play market backstack, After pressing back button,
+                        // to taken back to our application, we need to add following flags to intent.
+                        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                                Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET |
+                                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                        try {
+                            startActivity(goToMarket);
+                        } catch (ActivityNotFoundException e) {
+                            startActivity(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("http://play.google.com/store/apps/details?id=de.geeksfactory.opacclient")));
+                        }
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton(R.string.close, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        return builder.create();
+    }
+}
--- opacclient/opacapp/src/main/java/de/geeksfactory/opacclient/frontend/MainActivity.java
+++ opacclient/opacapp/src/main/java/de/geeksfactory/opacclient/frontend/MainActivity.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
@@ -21,6 +22,8 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import de.geeksfactory.opacclient.R;
@@ -137,6 +140,8 @@ public class MainActivity extends OpacActivity
         if (app.getLibrary() != null) {
             getSupportActionBar().setSubtitle(app.getLibrary().getDisplayName());
         }
+
+        showCelebDialog();
     }
 
     @Override
@@ -371,6 +376,23 @@ public class MainActivity extends OpacActivity
             } catch (Exception e) {
 
             }
+        }
+    }
+
+    public void showCelebDialog() {
+        if (!getApplicationContext().getPackageName().startsWith("de.geeksfactory.opacclient")) {
+            return;  // Never show e.g. in plus edition
+        }
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016, 3, 1, 0, 0, 0);
+        if ((new Date()).after(cal.getTime())) {
+            return;
+        }
+        if (!sp.contains("seen_celeb_dialog_20160201")) {
+            DialogFragment newFragment = new CelebrationDialogFragment();
+            newFragment.show(getSupportFragmentManager(), "celeb");
+            sp.edit().putBoolean("seen_celeb_dialog_20160201", true).commit();
         }
     }
 
