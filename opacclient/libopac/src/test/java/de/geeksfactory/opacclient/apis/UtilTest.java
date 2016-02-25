d865cbfa7b981c4bde0fcac92cf73a4c057116df
Johan von Forstner <johan.forstner@gmail.com>
2016-02-25 18:01:54 +0100

    Move UtilTest to libopac

--- opacclient/libopac/src/test/java/de/geeksfactory/opacclient/apis/UtilTest.java
+++ opacclient/libopac/src/test/java/de/geeksfactory/opacclient/apis/UtilTest.java
@@ -1,12 +1,10 @@
-package de.geeksfactory.opacclient.tests;
+package de.geeksfactory.opacclient.apis;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.junit.Test;
 
 import java.util.Arrays;
-
-import de.geeksfactory.opacclient.apis.BaseApi;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
