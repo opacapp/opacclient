cd416eb1d2fbca14e085e3056cd1d4a646443b65
Johan von Forstner <johan.forstner@gmail.com>
2016-03-19 19:11:20 +0100

    Add Gradle task for downloading JSON files Let's Encrypt SSL certificate needs to be added as Java 7 does not trust it by default

---
+++ opacclient/buildSrc/src/main/groovy/de/geeksfactory/opacclient/gradle/JsonFilesTask.groovy
@@ -0,0 +1,47 @@
+package de.geeksfactory.opacclient.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.util.GFileUtils
+import org.json.JSONArray
+import org.json.JSONObject
+
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManagerFactory
+import java.security.KeyStore
+
+class JsonFilesTask extends DefaultTask {
+    private static final String API_URL = "https://info.opacapp.net/androidconfigs/?format=json"
+    private static final String BIBS_DIR = "opacapp/src/main/assets/bibs"
+
+    @TaskAction
+    def downloadFiles() {
+        HttpsURLConnection conn = (HttpsURLConnection) API_URL.toURL().openConnection()
+        conn.setSSLSocketFactory(createSSLSocketFactory())
+        String response = conn.getInputStream().getText()
+        JSONArray data = new JSONArray(response)
+        GFileUtils.cleanDirectory(new File(BIBS_DIR))
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject library = data.get(i);
+            String id = library.getString("_id")
+            library.remove("_id")
+            File file = new File(BIBS_DIR, id + ".json")
+            file.write(library.toString())
+        }
+    }
+
+    private SSLSocketFactory createSSLSocketFactory() {
+        // Let's Encrypt certificates are not (yet?) trusted by Java JDK :(
+        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType())
+        keystore.load(getClass().getClassLoader().getResourceAsStream("letsencrypt_x3.jks"),
+                "FVVMHEdautxVcb9h4WdeZdMmvUwUHzgh".toCharArray())
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509")
+        tmf.init(keystore)
+        SSLContext context = SSLContext.getInstance("TLS")
+        context.init(null, tmf.getTrustManagers(), null)
+        SSLSocketFactory factory = context.getSocketFactory()
+        return factory
+    }
+}
