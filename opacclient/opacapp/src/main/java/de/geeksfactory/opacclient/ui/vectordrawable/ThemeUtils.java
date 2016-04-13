8354c243dd7b9f7775aea23f7cbac690171dcab0
johan12345 <johan.forstner@gmail.com>
2016-04-13 15:40:16 +0200

    Replace a bunch of icons with VectorDrawables

---
+++ opacclient/opacapp/src/main/java/de/geeksfactory/opacclient/ui/vectordrawable/ThemeUtils.java
@@ -0,0 +1,9 @@
+package de.geeksfactory.opacclient.ui.vectordrawable;
+
+/**
+ * Copied from AOSP code
+ */
+public class ThemeUtils {
+    static final int[] CHECKED_STATE_SET = new int[]{android.R.attr.state_checked};
+    static final int[] EMPTY_STATE_SET = new int[0];
+}
