152aa41d158210479cb0793b046b191b87506ff0
Raphael Michel <mail@raphaelmichel.de>
2016-06-21 10:09:38 +0200

    Fixed release build problems with OkHttp

--- opacclient/opacapp/src/release/java/de/geeksfactory/opacclient/utils/DebugTools.java
+++ opacclient/opacapp/src/release/java/de/geeksfactory/opacclient/utils/DebugTools.java
@@ -2,6 +2,8 @@ package de.geeksfactory.opacclient.utils;
 
 import android.app.Application;
 
+import okhttp3.OkHttpClient;
+
 public class DebugTools {
     public static void init(Application app) {
         // this is only used in the debug version of the app
