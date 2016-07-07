853ac21808946bcd122c544f6ef0ca04426443f3
johan12345 <johan.forstner@gmail.com>
2016-07-07 15:35:03 +0200

    ReminderHelper: Add shouldUpdateAlarmForChangedItemIds test

--- opacclient/opacapp/src/test/java/de/geeksfactory/opacclient/reminder/ReminderHelperTest.java
+++ opacclient/opacapp/src/test/java/de/geeksfactory/opacclient/reminder/ReminderHelperTest.java
@@ -5,13 +5,14 @@ import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 
 import java.util.ArrayList;
 
@@ -22,6 +23,7 @@ import de.geeksfactory.opacclient.storage.AccountDataSource;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -56,7 +58,7 @@ public class ReminderHelperTest {
 
     @Test
     public void shouldOnlyResetNotifiedOnReminderOff() throws Exception {
-        when(sp.getBoolean(eq("notification_service"), anyBoolean())).thenReturn(false);
+        setNotificationsEnabled(false);
 
         rh.generateAlarms();
         verify(data, times(1)).resetNotifiedOnAllAlarams();
@@ -65,7 +67,7 @@ public class ReminderHelperTest {
 
     @Test
     public void shouldNotCreateAlarmsWithoutLentItems() throws Exception {
-        when(sp.getBoolean(eq("notification_service"), anyBoolean())).thenReturn(true);
+        setNotificationsEnabled(true);
         when(data.getAllLentItems()).thenReturn(new ArrayList<LentItem>());
 
         rh.generateAlarms();
@@ -75,18 +77,12 @@ public class ReminderHelperTest {
 
     @Test
     public void shouldCreateAlarmForLentItem() throws Exception {
-        when(sp.getBoolean(eq("notification_service"), anyBoolean())).thenReturn(true);
-        ArrayList<LentItem> lentItems = new ArrayList<>();
+        setNotificationsEnabled(true);
 
-        LentItem item = new LentItem();
         LocalDate date = LocalDate.now();
         long dbId = 1L;
 
-        item.setDeadline(date);
-        item.setDbId(dbId);
-
-        lentItems.add(item);
-        when(data.getAllLentItems()).thenReturn(lentItems);
+        stubLentItem(date, dbId);
 
         rh.generateAlarms();
         verify(data, times(1)).addAlarm(date, new long[]{dbId},
@@ -95,23 +91,32 @@ public class ReminderHelperTest {
 
     @Test
     public void shouldRemoveAlarmForRemovedItem() throws Exception {
-        when(sp.getBoolean(eq("notification_service"), anyBoolean())).thenReturn(true);
+        setNotificationsEnabled(true);
         when(data.getAllLentItems()).thenReturn(new ArrayList<LentItem>());
 
-        ArrayList<Alarm> alarms = new ArrayList<>();
-        Alarm alarm = new Alarm();
-        alarm.deadline = LocalDate.now();
-        alarm.finished = false;
-        alarm.notified = false;
-        alarm.id = 1L;
-        alarm.media = new long[]{1L};
-        alarm.notificationTime = DateTime.now();
-        alarms.add(alarm);
-
-        when(data.getAllAlarms()).thenReturn(alarms);
+        Alarm alarm = stubAlarm();
 
         rh.generateAlarms();
         verify(data, times(1)).removeAlarm(alarm);
+    }
+
+    @Test
+    public void shouldUpdateAlarmForChangedItemIds() throws Exception {
+        setNotificationsEnabled(true);
+
+        stubLentItem(LocalDate.now(), 2L);
+        stubAlarm();
+
+        rh.generateAlarms();
+
+        verify(data, times(1)).updateAlarm(argThat(new ArgumentMatcher<Alarm>() {
+
+            @Override
+            public boolean matches(Object argument) {
+                Alarm alarm = (Alarm) argument;
+                return alarm.media.length == 1 && alarm.media[0] == 2L;
+            }
+        }));
     }
 
     @SuppressLint("CommitPrefEdits")
@@ -130,8 +135,36 @@ public class ReminderHelperTest {
         verifyNoMoreInteractions(editor);
     }
 
-    @After
-    public void tearDown() throws Exception {
+    private void setNotificationsEnabled(boolean on) {
+        when(sp.getBoolean(eq("notification_service"), anyBoolean())).thenReturn(on);
+    }
 
+    private LentItem stubLentItem(LocalDate date, long dbId) {
+        ArrayList<LentItem> lentItems = new ArrayList<>();
+        LentItem item = new LentItem();
+
+        item.setDeadline(date);
+        item.setDbId(dbId);
+
+        lentItems.add(item);
+        when(data.getAllLentItems()).thenReturn(lentItems);
+        return item;
+    }
+
+    @NonNull
+    private Alarm stubAlarm() {
+        ArrayList<Alarm> alarms = new ArrayList<>();
+        Alarm alarm = new Alarm();
+        alarm.deadline = LocalDate.now();
+        alarm.finished = false;
+        alarm.notified = false;
+        alarm.id = 1L;
+        alarm.media = new long[]{1L};
+        alarm.notificationTime = DateTime.now();
+        alarms.add(alarm);
+
+        when(data.getAllAlarms()).thenReturn(alarms);
+        when(data.getAlarmByDeadline(alarm.deadline)).thenReturn(alarm);
+        return alarm;
     }
 }
