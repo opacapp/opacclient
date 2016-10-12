e53d92f559d92cd7317c4e2464a21bfaddba3808
Raphael Michel <mail@raphaelmichel.de>
2016-10-12 15:52:32 +0200

    Revert "Fixed LibraryListActivity that is shadowed by WelcomeActivity"
    This reverts commit 0140b947706362a65c4f590412ab9b0a64255572.

--- opacclient/opacapp/src/main/java/de/geeksfactory/opacclient/frontend/LibraryListActivity.java
+++ opacclient/opacapp/src/main/java/de/geeksfactory/opacclient/frontend/LibraryListActivity.java
@@ -88,7 +88,6 @@ public class LibraryListActivity extends AppCompatActivity
     protected LibraryListFragment fragment3;
     protected LibraryListFragment fragment4;
     protected boolean visible;
-    protected boolean destroyed = false;
 
     protected AppCompatProgressDialog dialog;
 
@@ -112,12 +111,6 @@ public class LibraryListActivity extends AppCompatActivity
             loadLibrariesTask.execute((OpacClient) getApplication());
         }
         super.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        destroyed = true;
-        super.onDestroy();
     }
 
     @Override
@@ -866,7 +859,7 @@ public class LibraryListActivity extends AppCompatActivity
                 }
             }
             if (libraries == null) return;
-            if (destroyed) return;
+            if (!visible) return;
 
             // Get the intent, verify the action and get the query
             Intent intent = getIntent();
