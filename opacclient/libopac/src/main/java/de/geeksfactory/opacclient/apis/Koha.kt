d4e3fd241d9c62774881ecf06dc802badc27496b
johan12345 <johan.forstner@gmail.com>
2018-10-04 17:27:06 +0200

    Koha: implement parsing of lent and reserved items and fees

--- opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/Koha.kt
+++ opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/Koha.kt
@@ -5,7 +5,11 @@ import de.geeksfactory.opacclient.objects.*
 import de.geeksfactory.opacclient.searchfields.SearchField
 import de.geeksfactory.opacclient.searchfields.SearchQuery
 import okhttp3.FormBody
+import org.joda.time.LocalDate
+import org.joda.time.LocalDateTime
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 class Koha : OkHttpBaseApi() {
     protected lateinit var sru: SRU
@@ -55,15 +59,66 @@ class Koha : OkHttpBaseApi() {
     }
 
     override fun account(account: Account): AccountData {
-        login(account)
-        return AccountData(account.id)
+        val doc = login(account)
+        val accountData = AccountData(account.id)
+
+        accountData.lent = parseItems(doc, ::LentItem, "#checkoutst")
+        accountData.reservations = parseItems(doc, ::ReservedItem, "#holdst")
+
+        val feesDoc = Jsoup.parse(httpGet("$baseurl/cgi-bin/koha/opac-account.pl", "UTF-8"))
+        accountData.pendingFees = parseFees(feesDoc)
+
+        return accountData
+    }
+
+    private fun parseFees(feesDoc: Document): String? {
+        val text = feesDoc.select("td.sum").text()
+        return if (!text.isBlank()) text else null
+    }
+
+    private fun <I : AccountItem> parseItems(doc: Document, constructor: () -> I, id: String): List<I> {
+        val lentTable = doc.select(id).first() ?: return emptyList()
+
+        return lentTable.select("tbody tr").map { row ->
+            val item = constructor()
+            row.select("td").forEach { col ->
+                val type = col.attr("class").split(" ")[0]
+                val content = col.text()
+                when (type) {
+                    "itype" -> item.format = content
+                    "title" -> item.title = content
+                    "date_due" -> if (item is LentItem) item.deadline = parseDate(col)
+                    "branch" -> if (item is LentItem) {
+                        item.lendingBranch = content
+                    } else if (item is ReservedItem) {
+                        item.branch = content
+                    }
+                    "expirationdate" -> if (item is ReservedItem) item.expirationDate = parseDate(col)
+                    "status" -> item.status = content
+                    "modify" -> if (item is ReservedItem) {
+                        item.cancelData = col.select("input[name=reserve_id]").attr("value")
+                    }
+                    // "call_no" -> Signatur
+                    // "fines" -> Gebühren (Ja/Nein)
+                    // "reserve_date" -> Reservierungsdatum
+                }
+            }
+            item
+        }
+    }
+
+    private fun parseDate(col: Element): LocalDate? {
+        val select = col.select("span[title]").first() ?: return null
+        val isoformat = select.attr("title")
+        // example: <span title="2018-11-02T23:59:00">
+        return LocalDateTime(isoformat).toLocalDate()
     }
 
     override fun checkAccountData(account: Account) {
         login(account)
     }
 
-    private fun login(account: Account) {
+    private fun login(account: Account): Document {
         val formBody = FormBody.Builder()
                 .add("koha_login_context", "opac")
                 .add("koha_login_context", "opac")  // sic! two times
@@ -73,6 +128,7 @@ class Koha : OkHttpBaseApi() {
         if (doc.select(".alert").size > 0) {
             throw OpacApi.OpacErrorException(doc.select(".alert").text())
         }
+        return doc
     }
 
     override fun getShareUrl(id: String?, title: String?): String {
