f96d43a09104ae543e89f824577308dd554593f3
johan12345 <johan.forstner@gmail.com>
2018-11-13 14:14:53 +0100

    netbiblio: start to implement search()

--- opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/NetBiblio.kt
+++ opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/NetBiblio.kt
@@ -1,5 +1,6 @@
 package de.geeksfactory.opacclient.apis
 
+import de.geeksfactory.opacclient.i18n.StringProvider
 import de.geeksfactory.opacclient.networking.HttpClientFactory
 import de.geeksfactory.opacclient.objects.*
 import de.geeksfactory.opacclient.searchfields.DropdownSearchField
@@ -9,7 +10,9 @@ import de.geeksfactory.opacclient.searchfields.TextSearchField
 import de.geeksfactory.opacclient.utils.get
 import de.geeksfactory.opacclient.utils.html
 import de.geeksfactory.opacclient.utils.text
+import okhttp3.FormBody
 import org.json.JSONException
+import org.json.JSONObject
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.TextNode
 
@@ -62,6 +65,7 @@ open class NetBiblio : OkHttpBaseApi() {
             when (panel["data-filterfieldtype"]) {
                 "Checkbox" -> {
                     val checkboxes = panel.select("input[type=checkbox]")
+                    val modeSelector = panel.select("input[type=radio][name$=-mode][checked]").first()
                     listOf(
                             DropdownSearchField().apply {
                                 id = checkboxes[0]["name"]
@@ -72,6 +76,12 @@ open class NetBiblio : OkHttpBaseApi() {
                                                     checkbox["value"],
                                                     checkbox.nextElementSibling().text.trim())
                                         }
+                                if (modeSelector != null) {
+                                    data = JSONObject().apply {
+                                        put("modeKey", modeSelector.attr("name"))
+                                        put("modeValue", modeSelector.attr("value"))
+                                    }
+                                }
                             }
                     )
                 }
@@ -93,8 +103,68 @@ open class NetBiblio : OkHttpBaseApi() {
         return textFields + filterFields
     }
 
-    override fun search(query: List<SearchQuery>): SearchRequestResult? {
-        return null
+    override fun search(query: List<SearchQuery>): SearchRequestResult {
+        val body = FormBody.Builder()
+
+        var textCount = 0
+        for (q in query) {
+            if (q.searchField is TextSearchField && !q.key.startsWith("Filter.")) {
+                if (q.value.isNullOrBlank()) continue
+                if (textCount == 1) {
+                    body.add("Request.SearchOperator", "AND")
+                } else if (textCount >= 2) {
+                    throw OpacApi.OpacErrorException(stringProvider.getString(StringProvider.COMBINATION_NOT_SUPPORTED))
+                }
+                body.add("Request.SearchTerm", q.value)
+                body.add("Request.SearchKey", q.key)
+                textCount ++
+            }
+        }
+        if (textCount == 0) {
+            body.add("Request.SearchTerm", "")
+            body.add("Request.SearchKey", "W")
+            textCount ++
+        }
+        if (textCount == 1) {
+            body.add("Request.SearchOperator", "AND")
+            body.add("Request.SearchTerm", "")
+            body.add("Request.SearchKey", "W")
+        }
+
+        for (q in query) {
+            if (q.searchField is TextSearchField && q.key.startsWith("Filter.")) {
+                body.add(q.key, q.value)
+            } else if (q.searchField is DropdownSearchField) {
+                if (q.searchField.data != null && q.searchField.data.has("modeKey")) {
+                    body.add(q.searchField.data.getString("modeKey"), q.searchField.data.getString("modeValue"))
+                }
+                body.add(q.key, q.value)
+            }
+        }
+
+        body.add("Request.PageSize", "25")
+
+        val doc = httpPost("$opacUrl/search/extended/submit", body.build(), ENCODING).html
+        return parseSearch(doc, 1)
+    }
+
+    protected fun parseSearch(doc: Document, page: Int): SearchRequestResult {
+        val countText = doc.select("wo-grid-meta-resultcount").text
+        val totalCount = Regex("\\d+").findAll(countText).last().value.toInt()
+
+        val results = doc.select(".wo-grid-table tbody tr").groupBy { row ->
+            // there are multiple rows for one entry. Their CSS IDs all have the same prefix
+            val id = row.attr("id")
+            Regex("wo-row_\\d+").find(id)!!.value
+        }.map { entry ->
+            val rows = entry.value
+            val titleRow = rows[0]
+            SearchResult().apply {
+                innerhtml = titleRow.select("td[style=font-size: 14px;] a").text
+            }
+        }
+
+        return SearchRequestResult(results, totalCount, page)
     }
 
     override fun filterResults(filter: Filter, option: Filter.Option): SearchRequestResult? {
