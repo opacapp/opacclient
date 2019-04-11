ecb10965d83fbb1fc40851eb66c0a5ddd3e2b472
Johan von Forstner <johan.forstner@gmail.com>
2019-04-11 13:03:34 +0200

    add logging

--- opacclient/opacapp/src/googlePlayServices/java/de/geeksfactory/opacclient/utils/GooglePlayTools.java
+++ opacclient/opacapp/src/googlePlayServices/java/de/geeksfactory/opacclient/utils/GooglePlayTools.java
@@ -2,6 +2,7 @@ package de.geeksfactory.opacclient.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.google.android.gms.security.ProviderInstaller;
 
@@ -11,12 +12,12 @@ public class GooglePlayTools {
         ProviderInstaller.installIfNeededAsync(appContext, new ProviderInstaller.ProviderInstallListener() {
             @Override
             public void onProviderInstalled() {
-
+                Log.d("OpacApp", "Google Play security provider installed.");
             }
 
             @Override
             public void onProviderInstallFailed(int i, Intent intent) {
-
+                Log.d("OpacApp", String.format("Google Play security provider failed to install. Error code: %d", i));
             }
         });
     }
