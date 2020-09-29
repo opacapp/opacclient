8f2cc2ce40d4b25ce8ff469e6d2f06d5baa5d8a2
StefRe <steffen.rehberg@googlemail.com>
2020-09-29 22:30:05 +0200

    Remove colon from test resource file name
    to enable build on Windows systems as colons are not allowed in Windows file names.

--- opacclient/libopac/src/test/java/de/geeksfactory/opacclient/apis/SLUBTest.kt
+++ opacclient/libopac/src/test/java/de/geeksfactory/opacclient/apis/SLUBTest.kt
@@ -887,7 +887,7 @@ class SLUBSearchFieldsMockTest : BaseHtmlTest() {
 
     @Test
     fun testParseSearchFields() {
-        val html = readResource("/slub/SLUB Dresden: Startseite.html")
+        val html = readResource("/slub/SLUB Dresden Startseite.html")
         Mockito.doReturn(html).`when`(slub).httpGet(Matchers.any(), Matchers.any())
 
         val searchFields = slub.parseSearchFields()
