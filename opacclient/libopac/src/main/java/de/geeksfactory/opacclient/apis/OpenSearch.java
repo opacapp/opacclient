258c4d3909e06e32641ccf72ec35b59c9193bfcb
Raphael Michel <mail@raphaelmichel.de>
2021-03-26 11:30:04 +0100

    OPEN: Fix bug assigning covers

--- opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/OpenSearch.java
+++ opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/OpenSearch.java
@@ -351,8 +351,10 @@ public class OpenSearch extends OkHttpBaseApi implements OpacApi {
                                     byte[] bytes = Base64.decode(
                                             coverUrl.substring(coverUrl.indexOf(",") + 1));
                                     result.setCoverBitmap(bytes);
-                                } else {
+                                } else if (!coverData.getJSONObject("d").isNull("CoverUrl")) {
                                     result.setCover(coverUrl);
+                                } else {
+                                    assignBestCover(result, queue).join();
                                 }
                             } else {
                                 assignBestCover(result, queue).join();
