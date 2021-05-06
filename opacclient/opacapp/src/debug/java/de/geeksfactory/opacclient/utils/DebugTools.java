ac773477d23d656b48dbaad9319f5c97c416bf30
Raphael Michel <mail@raphaelmichel.de>
2021-05-06 13:44:24 +0200

    Replace stetho with flipper

--- opacclient/opacapp/src/debug/java/de/geeksfactory/opacclient/utils/DebugTools.java
+++ opacclient/opacapp/src/debug/java/de/geeksfactory/opacclient/utils/DebugTools.java
@@ -2,17 +2,38 @@ package de.geeksfactory.opacclient.utils;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
-import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.facebook.flipper.android.AndroidFlipperClient;
+import com.facebook.flipper.android.utils.FlipperUtils;
+import com.facebook.flipper.core.FlipperClient;
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin;
+import com.facebook.flipper.plugins.inspector.DescriptorMapping;
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor;
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin;
+import com.facebook.soloader.SoLoader;
 
+import de.geeksfactory.opacclient.BuildConfig;
 import okhttp3.OkHttpClient;
 
 public class DebugTools {
+    static NetworkFlipperPlugin networkPlugin;
+
     public static void init(Application app) {
-        Stetho.initializeWithDefaults(app);
+        SoLoader.init(app, false);
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(app)) {
+            final FlipperClient client = AndroidFlipperClient.getInstance(app);
+            client.addPlugin(new InspectorFlipperPlugin(app, DescriptorMapping.withDefaults()));
+            networkPlugin = new NetworkFlipperPlugin();
+            client.addPlugin(networkPlugin);
+            client.addPlugin(new DatabasesFlipperPlugin(app));
+
+            client.start();
+        }
     }
 
     public static OkHttpClient.Builder prepareHttpClient(OkHttpClient.Builder builder) {
-        return builder.addNetworkInterceptor(new StethoInterceptor());
+
+        return builder.addNetworkInterceptor(new FlipperOkhttpInterceptor(networkPlugin));
     }
 }
