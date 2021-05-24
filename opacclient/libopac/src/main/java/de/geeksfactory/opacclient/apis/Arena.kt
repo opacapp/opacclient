1151c18a47656095deb0eb90cfab073597ca29ca
Steffen Rehberg <steffen.rehberg@googlemail.com>
2021-05-24 13:11:55 +0200

    Fix double return statement (#627)
    it was introduced in 99812b4 by mistake

--- opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/Arena.kt
+++ opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/Arena.kt
@@ -356,7 +356,7 @@ open class Arena : OkHttpBaseApi() {
 
     override fun prolongMultiple(media: List<String>,
                                  account: Account, useraction: Int, selection: String?): OpacApi.ProlongAllResult {
-        return return OpacApi.ProlongAllResult(OpacApi.MultiStepResult.Status.UNSUPPORTED)
+        return OpacApi.ProlongAllResult(OpacApi.MultiStepResult.Status.UNSUPPORTED)
     }
 
     override fun cancel(media: String, account: Account, useraction: Int, selection: String?): OpacApi.CancelResult {
