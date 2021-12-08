a48ad0d79ef44ee5c9aff6fc1d60b81eeb5e53b2
Raphael Michel <mail@raphaelmichel.de>
2021-12-08 13:24:16 +0100

    Add some account features

--- opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/Lissy.kt
+++ opacclient/libopac/src/main/java/de/geeksfactory/opacclient/apis/Lissy.kt
@@ -7,8 +7,12 @@ import de.geeksfactory.opacclient.utils.get
 import de.geeksfactory.opacclient.utils.html
 import de.geeksfactory.opacclient.utils.text
 import okhttp3.FormBody
+import org.joda.time.LocalDate
+import org.joda.time.LocalDateTime
+import org.joda.time.format.DateTimeFormat
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 /**
  * OpacApi implementation for the BIBLIS LISSY OPAC.
@@ -92,6 +96,17 @@ open class Lissy : OkHttpBaseApi() {
         else return "$current $operator $searchParam"
     }
 
+    private fun followJsRedirect(waitpage: Document): Document {
+        val js = waitpage.select("head script")[0].data()
+        val match = Regex("window\\.location\\.replace\\(\"(.*)\"\\)").find(js)
+                ?: throw OpacApi.OpacErrorException("Could not find redirect")
+
+        // After the redirect we have a frameset again
+        val res = httpGet(baseurl + match.groupValues[1], ENCODING).html
+        res.setBaseUri(baseurl + match.groupValues[1])
+        return res
+    }
+
     private var searchPageUrl: String? = null
 
     override fun search(query: List<SearchQuery>): SearchRequestResult {
@@ -115,16 +130,7 @@ open class Lissy : OkHttpBaseApi() {
         }.build()
 
         // Submit search
-        val waitpage = httpPost(form.select("form[name=inputform]")[0].absUrl("action"), formData, ENCODING).html
-
-        // LISSY responds with a JS-based redirect
-        val js = waitpage.select("head script")[0].data()
-        val match = Regex("window\\.location\\.replace\\(\"(.*)\"\\)").find(js)
-                ?: throw OpacApi.OpacErrorException("Could not find redirect")
-
-        // After the redirect we have a frameset again
-        val resultframeset = httpGet(baseurl + match.groupValues[1], ENCODING).html
-        resultframeset.setBaseUri(baseurl + match.groupValues[1])
+        val resultframeset = followJsRedirect(httpPost(form.select("form[name=inputform]")[0].absUrl("action"), formData, ENCODING).html)
         val pageframeUrl = resultframeset.select("frame")[1].absUrl("src")
         val doc = httpGet(pageframeUrl, ENCODING).html
         doc.setBaseUri(pageframeUrl)
@@ -185,27 +191,13 @@ open class Lissy : OkHttpBaseApi() {
 
     override fun searchGetPage(page: Int): SearchRequestResult {
         val url = searchPageUrl!!.replace("pgnum=2", "pgnum=$page")
-        val waitpage = httpGet(url, ENCODING).html
-        // LISSY responds with a JS-based redirect
-        val js = waitpage.select("head script")[0].data()
-        val match = Regex("window\\.location\\.replace\\(\"(.*)\"\\)").find(js)
-                ?: throw OpacApi.OpacErrorException("Could not find redirect")
-        val doc = httpGet(baseurl + match.groupValues[1], ENCODING).html
-        doc.setBaseUri(baseurl + match.groupValues[1])
+        val doc = followJsRedirect(httpGet(url, ENCODING).html)
         return parseSearch(doc, page)
     }
 
     override fun getResultById(id: String, homebranch: String?): DetailedItem {
         // id is an URL
-        val waitpage = httpGet(id, ENCODING).html
-
-        // LISSY responds with a JS-based redirect
-        val js = waitpage.select("head script")[0].data()
-        val match = Regex("window\\.location\\.replace\\(\"(.*)\"\\)").find(js)
-                ?: throw OpacApi.OpacErrorException("Could not find redirect")
-
-        val doc = httpGet(baseurl + match.groupValues[1], ENCODING).html
-        doc.setBaseUri(baseurl + match.groupValues[1])
+        val doc = followJsRedirect(httpGet(id, ENCODING).html)
         return parseDetail(doc)
     }
 
@@ -237,37 +229,192 @@ open class Lissy : OkHttpBaseApi() {
         return null
     }
 
-    override fun reservation(item: DetailedItem, account: Account, useraction: Int, selection: String?): OpacApi.ReservationResult {
-        TODO()
-    }
+    data class AccountMenu(
+            val lentUrl: String,
+            val reservationsUrl: String
+    )
 
-    override fun prolong(media: String, account: Account, useraction: Int, selection: String?): OpacApi.ProlongResult {
-        TODO()
-    }
+    private fun login(account: Account): AccountMenu {
+        val frameset = httpGet("$baseurl/lissy/lissy.ly?pg=bnrlogin", ENCODING).html
+        frameset.setBaseUri("$baseurl/lissy/lissy.ly?pg=bnrlogin")
+        val topframeUrl = frameset.select("frame")[0].absUrl("src")
+        val topframe = httpGet(topframeUrl, ENCODING).html
+        topframe.setBaseUri(topframeUrl)
+        val pageframeUrl = topframe.select("frame")[1].absUrl("src")
+        val pageframe = httpGet(pageframeUrl, ENCODING).html
+        pageframe.setBaseUri(pageframeUrl)
 
-    override fun prolongAll(account: Account, useraction: Int, selection: String?): OpacApi.ProlongAllResult {
-        TODO()
-    }
+        val formData = FormBody.Builder().apply {
+            pageframe.select("form[name=form1] input[type=hidden]").forEach { hidden ->
+                add(hidden["name"], hidden["value"])
+            }
+            add("bnr", account.name)
+            add("gd", account.password)
+        }.build()
 
-    override fun prolongMultiple(media: List<String>,
-                                 account: Account, useraction: Int, selection: String?): OpacApi.ProlongAllResult {
-        return OpacApi.ProlongAllResult(OpacApi.MultiStepResult.Status.UNSUPPORTED)
-    }
+        val postUrl = pageframe.select("form[name=form1]").first().absUrl("action")
+        val lframeset = httpPost(postUrl, formData, ENCODING).html
+        lframeset.setBaseUri(postUrl)
+        val ltopframeUrl = lframeset.select("frame")[0].absUrl("src")
+        val ltopframe = httpGet(ltopframeUrl, ENCODING).html
+        ltopframe.setBaseUri(ltopframeUrl)
+        val lpageframeUrl = ltopframe.select("frame")[1].absUrl("src")
+        val lpageframe = httpGet(lpageframeUrl, ENCODING).html
+        lpageframe.setBaseUri(lpageframeUrl)
 
-    override fun cancel(media: String, account: Account, useraction: Int, selection: String?): OpacApi.CancelResult {
-        TODO()
-    }
-
-    override fun account(account: Account): AccountData {
-        TODO()
+        if (lpageframe.select("table").size == 0) {
+            throw OpacApi.OpacErrorException(lpageframe.select("body").text())
+        }
+        val lmenuframeUrl = ltopframe.select("frame")[0].absUrl("src")
+        val lmenuframe = httpGet(lmenuframeUrl, ENCODING).html
+        lmenuframe.setBaseUri(lmenuframeUrl)
+        return AccountMenu(
+                lentUrl = lmenuframe.select("a").find { it.attr("href").contains("type=entl") }?.absUrl("href")
+                        ?: throw OpacApi.OpacErrorException("Lent items not found in menu"),
+                reservationsUrl = lmenuframe.select("a").find { it.attr("href").contains("type=vorg") }?.absUrl("href")
+                        ?: throw OpacApi.OpacErrorException("Lent items not found in menu")
+        )
     }
 
     override fun checkAccountData(account: Account) {
         login(account)
     }
 
-    private fun login(account: Account): Document {
+    override fun account(account: Account): AccountData {
+        val menu = login(account)
+        val dateFormat = DateTimeFormat.forPattern("dd.MM.yyyy")
+
+        return AccountData(account.id).apply {
+            lent = mutableListOf()
+            val lentDoc = followJsRedirect(httpGet(menu.lentUrl, ENCODING).html)
+            lentDoc.select("form table").first()?.select("tr")?.forEachIndexed { idx, tr ->
+                if (idx == 0) return@forEachIndexed  // Header row
+
+                lent.add(LentItem().apply {
+                    barcode = tr.child(2).text().trim()
+                    title = tr.child(3).text().trim()
+                    deadline = dateFormat.parseLocalDate(tr.child(4).text().replace(Regex("[^0-9.]"), ""))
+                    isRenewable = tr.child(0).select("input").first() != null
+                    prolongData = if (isRenewable) barcode else null
+                })
+            }
+
+            reservations = mutableListOf()
+            val resDoc = httpGet(menu.reservationsUrl, ENCODING).html
+            resDoc.select("form table").first()?.select("tr")?.forEachIndexed { idx, tr ->
+                if (idx == 0) return@forEachIndexed  // Header row
+
+                reservations.add(ReservedItem().apply {
+                    title = tr.child(5).text().trim()
+                    val ready = tr.child(6).text().replace(Regex("[^0-9.]"), "")
+                    readyDate = if (ready.isBlank()) dateFormat.parseLocalDate(ready) else null
+                    val expiry = tr.child(7).text().replace(Regex("[^0-9.]"), "")
+                    expirationDate = if (ready.isBlank()) dateFormat.parseLocalDate(expiry) else null
+                    cancelData = tr.child(4).text().trim()
+                })
+            }
+        }
+    }
+
+    override fun reservation(item: DetailedItem, account: Account, useraction: Int, selection: String?): OpacApi.ReservationResult {
         TODO()
+    }
+
+    override fun prolong(media: String, account: Account, useraction: Int, selection: String?): OpacApi.ProlongResult {
+        val menu = login(account)
+        val lentDoc = followJsRedirect(httpGet(menu.lentUrl, ENCODING).html)
+
+        val tr = lentDoc.select("form table").first().select("tr").find { it ->
+            media == it.child(2).text().trim()
+        } ?: return OpacApi.ProlongResult(OpacApi.MultiStepResult.Status.ERROR, "Medium not found")
+
+        val form = lentDoc.select("form[name=form1]").first()
+
+        val formData = FormBody.Builder().apply {
+            form.select("input[type=hidden]").forEach { field ->
+                add(field["name"], field["value"])
+            }
+            tr.select("input[type=checkbox]").forEach { field ->
+                add(field["name"], field["value"])
+            }
+        }.build()
+        val resultframe = httpPost(form.absUrl("action"), formData, ENCODING).html
+        resultframe.setBaseUri(form.absUrl("action"))
+        val resultUrl = resultframe.select("frame")[1].absUrl("src")
+        val result = httpGet(resultUrl, ENCODING).html
+        result.setBaseUri(resultUrl)
+        if (result.select("table tr")[1].child(3).text().trim() == "Nein") {
+            return OpacApi.ProlongResult(OpacApi.MultiStepResult.Status.ERROR, result.select("table tr")[1].child(4).text().trim())
+        }
+        return OpacApi.ProlongResult(OpacApi.MultiStepResult.Status.OK)
+    }
+
+    override fun prolongAll(account: Account, useraction: Int, selection: String?): OpacApi.ProlongAllResult {
+        return prolongMultiple(null, account, useraction, selection)
+    }
+
+    override fun prolongMultiple(media: List<String>?,
+                                 account: Account, useraction: Int, selection: String?): OpacApi.ProlongAllResult {
+        val menu = login(account)
+        val lentDoc = followJsRedirect(httpGet(menu.lentUrl, ENCODING).html)
+
+        val trs = lentDoc.select("form table").first().select("tr").filter { it ->
+            media == null || media.contains(it.child(2).text().trim())
+        }
+        if (media != null && trs.size != media.size)
+            return OpacApi.ProlongAllResult(OpacApi.MultiStepResult.Status.ERROR, "Medium not found")
+
+        val form = lentDoc.select("form[name=form1]").first()
+
+        val formData = FormBody.Builder().apply {
+            form.select("input[type=hidden]").forEach { field ->
+                add(field["name"], field["value"])
+            }
+            trs.forEach {
+                it.select("input[type=checkbox]").forEach { field ->
+                    add(field["name"], field["value"])
+                }
+            }
+        }.build()
+        val resultframe = httpPost(form.absUrl("action"), formData, ENCODING).html
+        resultframe.setBaseUri(form.absUrl("action"))
+        val resultUrl = resultframe.select("frame")[1].absUrl("src")
+        val result = httpGet(resultUrl, ENCODING).html
+        result.setBaseUri(resultUrl)
+        val list = result.select("table tr").drop(1).map {
+            mapOf(
+                    OpacApi.ProlongAllResult.KEY_LINE_TITLE to it.child(1).text().trim(),
+                    OpacApi.ProlongAllResult.KEY_LINE_NR to it.child(0).text().trim(),
+                    OpacApi.ProlongAllResult.KEY_LINE_NEW_RETURNDATE to it.child(2).text().trim(),
+                    OpacApi.ProlongAllResult.KEY_LINE_MESSAGE to it.child(4).text().trim()
+            )
+        }
+
+        return OpacApi.ProlongAllResult(OpacApi.MultiStepResult.Status.OK, list)
+    }
+
+    override fun cancel(media: String, account: Account, useraction: Int, selection: String?): OpacApi.CancelResult {
+        val menu = login(account)
+        val resDoc = httpGet(menu.reservationsUrl, ENCODING).html
+        resDoc.setBaseUri(menu.reservationsUrl)
+
+        val tr = resDoc.select("form table").first().select("tr").find { it ->
+            it.child(4).text().trim() == media
+        } ?: return OpacApi.CancelResult(OpacApi.MultiStepResult.Status.ERROR, "Medium not found")
+
+        val form = resDoc.select("form[name=form1]").first()
+
+        val formData = FormBody.Builder().apply {
+            form.select("input[type=hidden]").forEach { field ->
+                add(field["name"], field["value"])
+            }
+            tr.select("input[type=checkbox]").forEach { field ->
+                add(field["name"], field["value"])
+            }
+        }.build()
+        val result = httpPost(form.absUrl("action"), formData, ENCODING).html
+        val msg = result.select("h4 + p > font").first()
+        return OpacApi.CancelResult(OpacApi.MultiStepResult.Status.OK, msg?.text())
     }
 
     override fun getShareUrl(id: String?, title: String?): String {
@@ -276,7 +423,7 @@ open class Lissy : OkHttpBaseApi() {
 
     override fun getSupportFlags(): Int {
         return OpacApi.SUPPORT_FLAG_ENDLESS_SCROLLING or OpacApi.SUPPORT_FLAG_ACCOUNT_PROLONG_ALL or
-                OpacApi.SUPPORT_FLAG_WARN_RESERVATION_FEES
+                OpacApi.SUPPORT_FLAG_WARN_RESERVATION_FEES or OpacApi.SUPPORT_FLAG_ACCOUNT_PROLONG_MULTIPLE
     }
 
     override fun getSupportedLanguages(): Set<String>? {
